50703b3151e8cdeb93f3c1ac83ff0892c5bc2d95
OPHELIA-PC\helia <heliashelium@gmail.com>
2022-04-16 18:16:50 -0500

    Add spawndynamicmob GM command | Fix wings dynamis module loading

--- scripts/commands/spawndynamicmob.lua
+++ scripts/commands/spawndynamicmob.lua
@@ -1,33 +1,31 @@
 -----------------------------------
--- func: !spawndynamicmob <Mob's Group ID> <Mob's Zone ID> {Number of Mobs} {Name for the Mob} {dropsEnabled (1/0} {Costume Finder (1/0)}
+-- func: spawndynamicmob <Mob's Group ID> <Mob's Zone ID> <Number of Mobs> <Name for the Mob>
 -- desc: Spawn a dynamic mob exactly matching that of a normal mob.
--- note: Original code from zach2Good's Fafnir.lua
+-- note:
 -----------------------------------
 
 cmdprops =
 {
-    permission = 4,
-    parameters = "iiiiisii"
+    permission = 2,
+    parameters = "iiisi"
 }
 
 function error(player, msg)
     player:PrintToPlayer(msg)
-    player:PrintToPlayer("!spawndynamicmob <Mob's Group ID> <Mob's Zone ID> {Number of Mobs} {Name for the Mob} {dropsEnabled (1/0} {Costume Finder (1/0)}")
+    player:PrintToPlayer("!spawndynamicmob <Mob's Group ID> <Mob's Zone ID> <Number of Mobs> <Name for the Mob> {dropsEnabled (0 == False, 1 == True)}")
 end
 
-function onTrigger(player, mobGroupID, mobZoneID, numberOfMob, mobLook, mobName, dropsEnabled, costumeFinder)
+function onTrigger(player, mobGroupID, mobZoneID, numberOfMob, mobName, dropsEnabled)
     local i = 1
-    local mobLookActive = mobLook
 
-    -- Make Mob Name Optional
     if mobName == nil then
-        mobName = "NPC"
-    else
-        mobName = mobName
+        error(player, "You must provide a name for the mob.")
+        return
     end
 
     if numberOfMob == nil then
-        numberOfMob = 1
+        error(player, "You must provide the number of mobs you want to spawn.")
+        return
     end
 
     if mobZoneID == nil then
@@ -40,72 +38,33 @@ function onTrigger(player, mobGroupID, mobZoneID, numberOfMob, mobLook, mobName,
         return
     end
 
-    if mobLook ~= nil then
-        while i <= numberOfMob do
+    while i <= numberOfMob do
+        local zone = player:getZone()
+        local mob = zone:insertDynamicEntity({
+            objtype = xi.objType.MOB,
+            name = mobName,
+            x = player:getXPos(),
+            y = player:getYPos(),
+            z = player:getZPos(),
+            rotation = player:getRotPos(),
+    
+            groupId = mobGroupID,
+            groupZoneId = mobZoneID,
+    
+            onMobDeath = function(mob, playerArg, isKiller)
+            end,
+        })
 
-            if numberOfMob > 1 and costumeFinder == 1 then
-                mobName = string.format("%i", mobLookActive)
-            end
+        -- Use the mob object as you normally would
+        mob:setSpawn(player:getXPos(), player:getYPos(), player:getZPos(), player:getRotPos())
 
-            local zone = player:getZone()
-            local mobTypeVar = xi.objType.MOB
-            local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = mobName,
-                x = player:getXPos(),
-                y = player:getYPos(),
-                z = player:getZPos(),
-                rotation = player:getRotPos(),
-                groupId = mobGroupID,
-                groupZoneId = mobZoneID,
-                look = mobLookActive,
-                onMobDeath = function(mob, playerArg, isKiller)
-                end,
-            })
-
-            -- Use the mob object as you normally would
-            mob:setSpawn(player:getXPos(), player:getYPos(), player:getZPos(), player:getRotPos())
-
-            if dropsEnabled == 0 or dropsEnabled == nil then
-                mob:setDropID(0) -- No loot!
-            end
-
-            mob:spawn()
-
-            --player:PrintToPlayer("Spawning: ".. mobName ..", Mob ID: ".. mob:getID() ..", Mob Main Level: ".. mob:getMainLvl())
-            player:PrintToPlayer(string.format("Spawning %s: (ID: %i) (Level: %i)", mob:getName(), mob:getID(), mob:getMainLvl()))
-            i = i + 1
-            mobLookActive = mobLookActive + 1
+        if dropsEnabled == 0 or dropsEnabled == nil then
+            mob:setDropID(0) -- No loot!
         end
-    else
-        while i <= numberOfMob do
-            local zone = player:getZone()
-            local mobTypeVar = xi.objType.MOB
-            local mob = zone:insertDynamicEntity({
-                objtype = xi.objType.MOB,
-                name = mobName,
-                x = player:getXPos(),
-                y = player:getYPos(),
-                z = player:getZPos(),
-                rotation = player:getRotPos(),
-                groupId = mobGroupID,
-                groupZoneId = mobZoneID,
-                onMobDeath = function(mob, playerArg, isKiller)
-                end,
-            })
 
-            -- Use the mob object as you normally would
-            mob:setSpawn(player:getXPos(), player:getYPos(), player:getZPos(), player:getRotPos())
+        mob:spawn()
 
-            if dropsEnabled == 0 or dropsEnabled == nil then
-                mob:setDropID(0) -- No loot!
-            end
-
-            mob:spawn()
-
-            --player:PrintToPlayer("Spawning: ".. mobName ..", Mob ID: ".. mob:getID() ..", Mob Main Level: ".. mob:getMainLvl())
-            player:PrintToPlayer(string.format("Spawning %s: (ID: %i) (Level: %i)", mob:getName(), mob:getID(), mob:getMainLvl()))
-            i = i + 1
-        end
+        player:PrintToPlayer("Spawning: ".. mobName ..", Mob ID: ".. mob:getID() ..", Mob Main Level: ".. mob:getMainLvl())
+        i = i + 1
     end
 end
