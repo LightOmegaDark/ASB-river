e68cae690f231554116ab7030230415fef5ea76b
Critical <lyardeen@gmail.com>
2022-05-27 10:53:12 -0600

    Add GM command to return enmity to player Co-authored-by: Twilight <23407689+NiQ1@users.noreply.github.com> Co-authored-by: Caelic <bclynn96@gmail.com> Co-authored-by: Velyn <ffxi@somepage.org>

--- scripts/commands/getenmity.lua
+++ scripts/commands/getenmity.lua
@@ -17,12 +17,14 @@ function error(player, msg)
 end
 
 function onTrigger(player)
+
     local targ = player:getCursorTarget()
 
-    if not targ or not targ:isMob() then
-        error(player, "You must select a target monster with the cursor first.")
+    if targ == nil or targ:isMob() == false then
+        error(player, "you must select a target monster with the cursor first")
         return
     end
 
-    player:PrintToPlayer(string.format("Your enmity against %s is ... CE = %u ... VE = %u", targ:getName(), targ:getCE(player), targ:getVE(player)))
+    player:PrintToPlayer(string.format("your enmity against %s is ... CE = %u ... VE = %u", targ:getName(), targ:getCE(player), targ:getVE(player)))
+
 end
