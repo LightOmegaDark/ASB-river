16150a1ba24b47666b6fce083eb76e1ab10320dc
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-06-15 22:14:53 -0400

    CoP 3-3 Balance Fixes & Antlion Ambush

--- scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
+++ scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
@@ -4,7 +4,24 @@
 -----------------------------------
 local entity = {}
 
-entity.onMobDeath = function(mob, player, optParams)
+entity.onMobInitialize = function(mob)
+    mob:addMod(xi.mod.DOUBLE_ATTACK, 10)
+    mob:addMod(xi.mod.REGAIN, 50)
+end
+
+entity.onMobFight = function(mob, target)
+    if mob:getHPP() <= 15 then
+        mob:setMod(xi.mod.REGAIN, 200)
+    end
+end
+
+entity.onMobWeaponSkillPrepare = function(mob, target)
+    if mob:getHPP() <= 15 then
+        return 319
+    end
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
 end
 
 return entity
