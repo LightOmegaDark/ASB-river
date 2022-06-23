494614fb34f4ada250eebd8d325ee85e9790bcc9
OPHELIA-PC\helia <heliashelium@gmail.com>
2022-06-23 12:14:45 -0500

    COP 8-3 Convert to Interaction

--- scripts/zones/AlTaieu/Zone.lua
+++ scripts/zones/AlTaieu/Zone.lua
@@ -16,23 +16,10 @@ zoneObject.onConquestUpdate = function(zone, updatetype)
     xi.conq.onConquestUpdate(zone, updatetype)
 end
 
-zoneObject.onZoneIn = function(player, prevZone)
-    local cs = -1
-
-    if
-        player:getXPos() == 0 and
-        player:getYPos() == 0 and
-        player:getZPos() == 0
-    then
+zone_object.onZoneIn = function(player, prevZone)
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
         player:setPos(-25, -1 , -620 , 33)
     end
-    if (player:getCurrentMission(xi.mission.log_id.COP) == xi.mission.id.cop.GARDEN_OF_ANTIQUITY and player:getCharVar("PromathiaStatus") == 0) then
-        cs=1
-    elseif (player:getCurrentMission(xi.mission.log_id.COP) == xi.mission.id.cop.DAWN and player:getCharVar("PromathiaStatus") == 0) then
-        cs=167
-    end
-
-    return cs
 end
 
 zoneObject.onTriggerAreaEnter = function(player, triggerArea)
@@ -41,12 +28,7 @@ end
 zoneObject.onEventUpdate = function(player, csid, option)
 end
 
-zoneObject.onEventFinish = function(player, csid, option)
-    if csid == 167 then
-        player:setCharVar("PromathiaStatus", 1)
-        player:delKeyItem(xi.ki.MYSTERIOUS_AMULET_PRISHE)
-        player:messageSpecial(ID.text.RETURN_AMULET_TO_PRISHE, xi.ki.MYSTERIOUS_AMULET)
-    end
+zone_object.onEventFinish = function(player, csid, option)
 end
 
 return zoneObject
