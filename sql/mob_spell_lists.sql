823a4c3727b3202e2784ed0abe27f9116b0a32ae
Critical <lyardeen@gmail.com>
2022-07-05 14:29:53 -0600

    Castole Oztroja and Attohwa Chasm NM audit

--- sql/mob_spell_lists.sql
+++ sql/mob_spell_lists.sql
@@ -5167,8 +5167,21 @@ INSERT INTO `mob_spell_lists` VALUES('Generic_Doll', 511, 194, 1, 255); -- Thund
 INSERT INTO `mob_spell_lists` VALUES('Generic_Doll', 511, 251, 1, 255); -- Shock Spikes (1~255)
 INSERT INTO `mob_spell_lists` VALUES('Generic_Doll', 511, 356, 1, 255); -- Paralyga (1~255)
 
--- Hamadryad (512)
-INSERT INTO `mob_spell_lists` VALUES('Hamadryad',512,145,1,255); -- fire_ii (1~255)
+-- Hazmat (512)
+INSERT INTO `mob_spell_lists` VALUES ('Hazmat',512,204,1,255); -- flare (1~255)
+
+-- Lii Jixa the Somnolist (513)
+INSERT INTO `mob_spell_lists` VALUES ('Lii_Jixa_the_Somnolist',513,3,1,255);  -- cure_iii (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Lii_Jixa_the_Somnolist',513,29,1,255); -- banish_ii (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Lii_Jixa_the_Somnolist',513,98,1,255); -- repose (1~255)
+
+-- Slendlix Spindlethumb (514)
+INSERT INTO `mob_spell_lists` VALUES ('Slendlix_Spindlethumb',514,3,1,255);  -- cure_iii (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Slendlix_Spindlethumb',514,23,1,255); -- dia (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Slendlix_Spindlethumb',514,33,1,255); -- diaga (1~255)
+
+-- Hamadryad (515)
+INSERT INTO `mob_spell_lists` VALUES('Hamadryad',515,145,1,255); -- fire_ii (1~255)
 
 -- AirSkyBoat Specific Starts at 5000
 -- 5000 -> 5010 is Era Dynamis
