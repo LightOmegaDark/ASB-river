32e64a8dba25ed579c0701e6ef63edb6469a530f
OpheliaXI <heliashelium@gmail.com>
2022-07-04 20:09:27 -0500

    Fix Startup Failing

--- src/search/search.cpp
+++ src/search/search.cpp
@@ -23,9 +23,12 @@ along with this program.  If not, see http://www.gnu.org/licenses/
 
 #include "common/blowfish.h"
 #include "common/cbasetypes.h"
+#include "common/console_service.h"
 #include "common/logging.h"
+#include "common/lua.h"
 #include "common/md52.h"
 #include "common/mmo.h"
+#include "common/settings.h"
 #include "common/socket.h"
 #include "common/sql.h"
 #include "common/taskmgr.h"
