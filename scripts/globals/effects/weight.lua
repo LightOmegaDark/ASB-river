e0aeac68fb7298922b49471b198266fd0cb659a8
OpheliaXI <heliashelium@gmail.com>
2022-07-10 21:19:05 -0500

    Fix Restoring onEffect Lose

--- scripts/globals/effects/weight.lua
+++ scripts/globals/effects/weight.lua
@@ -15,7 +15,7 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.MOVE, -effect:getPower())
-    target:addMod(xi.mod.EVA, 10)
+    target:delMod(xi.mod.EVA, -10)
 end
 
 return effectObject
