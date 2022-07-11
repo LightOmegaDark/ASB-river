50f0b069b5c6a1f27cb7a38b05b752fc02ada6cc
Critical <lyardeen@gmail.com>
2022-07-10 19:29:06 -0600

    The Sanctuary of Zitah NM Audit

--- scripts/zones/The_Sanctuary_of_ZiTah/mobs/Bastet.lua
+++ scripts/zones/The_Sanctuary_of_ZiTah/mobs/Bastet.lua
@@ -6,7 +6,12 @@ require("scripts/globals/hunts")
 -----------------------------------
 local entity = {}
 
-entity.onMobDeath = function(mob, player, optParams)
+entity.onMobSpawn = function(mob)
+    mob:setMod(xi.mod.MOVE, 12)
+    mob:setMobMod(xi.mobMod.NO_STANDBACK, 1)
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
     xi.hunts.checkHunt(mob, player, 325)
 end
 
