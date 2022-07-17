30dc400ed596f3487a3fe4a439e3320e1c7ce512
OpheliaXI <heliashelium@gmail.com>
2022-07-17 00:50:55 -0500

    Fix Avatar ATK | Add WSC Calc to Physical and Magical Bloodpacts

--- scripts/globals/abilities/pets/nether_blast.lua
+++ scripts/globals/abilities/pets/nether_blast.lua
@@ -12,14 +12,13 @@ abilityObject.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
 
-abilityObject.onPetAbility = function(target, pet, skill)
-    local level = pet:getMainLvl()
-    local damage = 5 * level + 10
+ability_object.onPetAbility = function(target, pet, skill)
+    local damage = pet:getMainLvl() + 2
     damage = xi.mobskills.mobMagicalMove(pet, target, skill, damage, xi.magic.ele.DARK, 5, xi.mobskills.magicalTpBonus.NO_EFFECT, 0)
     damage = xi.mobskills.mobAddBonuses(pet, target, damage.dmg, xi.magic.ele.DARK)
-    damage = xi.summon.avatarFinalAdjustments(damage, pet, skill, target, xi.attackType.MAGICAL, xi.damageType.DARK, 1)
+    damage = xi.summon.avatarFinalAdjustments(damage, pet, skill, target, xi.attackType.BREATH, xi.damageType.DARK, 1)
 
-    target:takeDamage(damage, pet, xi.attackType.MAGICAL, xi.damageType.DARK)
+    target:takeDamage(damage, pet, xi.attackType.BREATH, xi.damageType.DARK)
     target:updateEnmityFromDamage(pet, damage)
 
     return damage
