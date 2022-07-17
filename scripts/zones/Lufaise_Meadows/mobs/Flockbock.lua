4e8705d6c2cfddf09bdf07f47cd80da99308e59a
Critical <lyardeen@gmail.com>
2022-07-16 19:29:53 -0600

    Lufaise Meadows NM Audit

--- scripts/zones/Lufaise_Meadows/mobs/Flockbock.lua
+++ scripts/zones/Lufaise_Meadows/mobs/Flockbock.lua
@@ -3,10 +3,21 @@
 --   NM: Flockbock
 -----------------------------------
 require("scripts/globals/hunts")
+mixins = {require("scripts/mixins/rage")}
 -----------------------------------
 local entity = {}
 
-entity.onMobDeath = function(mob, player, optParams)
+-- Confirm spawn positions
+
+entity.onMobInitialize = function(mob)
+    mob:addMod(xi.mod.MOVE, 10)
+end
+
+entity.onMobSpawn = function(mob)
+    mob:setLocalVar("[rage]timer", 3600) -- 1 hour rage
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
     xi.hunts.checkHunt(mob, player, 442)
 end
 
