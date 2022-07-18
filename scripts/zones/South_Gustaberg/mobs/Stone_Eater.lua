b62b53ba76dbf5c11d1c3dd783969ceef4aa9a7e
OpheliaXI <heliashelium@gmail.com>
2022-07-17 19:51:36 -0500

    Make Var More Specific

--- scripts/zones/South_Gustaberg/mobs/Stone_Eater.lua
+++ scripts/zones/South_Gustaberg/mobs/Stone_Eater.lua
@@ -6,7 +6,11 @@ require("scripts/globals/regimes")
 -----------------------------------
 local entity = {}
 
-entity.onMobDeath = function(mob, player, optParams)
+entity.onMobEngaged = function(mob, target)
+    mob:setLocalVar("[CastTime]ID_159", 10)
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
     xi.regime.checkRegime(player, mob, 77, 1, xi.regime.type.FIELDS)
 end
 
