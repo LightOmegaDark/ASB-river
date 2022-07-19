5a132fdeb1175da721f655ccd3d4cec48cbbd0cc
Ophelia <84217630+OpheliaXI@users.noreply.github.com>
2022-07-18 21:31:41 -0500

    Add MAGIC_INTERRUPTED State Listener (#153)

--- src/map/ai/states/magic_state.cpp
+++ src/map/ai/states/magic_state.cpp
@@ -223,7 +223,8 @@ bool CMagicState::Update(time_point tick)
 
         if (m_interrupted)
         {
-            m_PEntity->OnCastInterrupted(*this, action, msg, false);
+            m_PEntity->OnCastInterrupted(*this, action, msg);
+            m_PEntity->PAI->EventHandler.triggerListener("MAGIC_INTERRUPTED", CLuaBaseEntity(m_PEntity), CLuaBaseEntity(PTarget), CLuaSpell(m_PSpell.get()), &action);
         }
         else
         {
