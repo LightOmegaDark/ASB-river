18c9ec185ccda7661b1fc06f40caf350bd0be663
dallano <atwood.dallan@hotmail.com>
2022-07-31 14:18:37 -0600

    Under Observation Fixes
    Basis for the following changes received from the following:
    https://ffxiclopedia.fandom.com/wiki/Under_Observation
    https://www.youtube.com/watch?v=JndRb6OAK-Q&ab_channel=InfiniteZero
    https://www.youtube.com/watch?v=jhMWT3ubNmM&ab_channel=Dandruffshampoo
    
    Fixes:
    Gave regain to all mobs in the battlefield
    Gave sleep immunity to Sobbing Eyes
    Gave silence resistance to Sobbing Eyes
    Added hate reset to Petrigaze. Timestamp in video 6:08, as well as reference to this behavior in wikipedia

--- scripts/zones/Horlais_Peak/mobs/Compound_Eyes.lua
+++ scripts/zones/Horlais_Peak/mobs/Compound_Eyes.lua
@@ -1,6 +1,6 @@
 -----------------------------------
 -- Area: Horlais Peak
---  Mob: Sobbing Eyes
+--  Mob: Combound Eyes
 -- BCNM: Under Observation
 -----------------------------------
 require("scripts/globals/status")
@@ -8,15 +8,7 @@ require("scripts/globals/status")
 local entity = {}
 
 entity.onMobInitialize = function(mob)
-    mob:setMod(xi.mod.SLEEPRES, 100)
-    mob:setMod(xi.mod.SILENCERES, 75)
     mob:setMod(xi.mod.REGAIN, 100)
-end
-
-entity.onMobWeaponSkill = function(target, mob, skill)
-    if skill:getID() == 439 then
-        mob:resetEnmity(target)
-    end
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
