48b95f0283b68c396ca7c3e27c0577a71b994e5c
dallano <atwood.dallan@hotmail.com>
2022-08-01 00:33:11 -0600

    3, 2, 1 Changes
    Co-Authored-By: CriticalXI <lyardeen@gmail.com>
    
    Evidence:
    https://www.youtube.com/watch?v=4mpC0UAo1qk&ab_channel=FinalFantasyMeditations
    
    Changes:
    Added messaging for timer countdown
    Increased size of Time Bomb
    Set time of self destruct to 0 to prevent it from being able to be stunned

--- scripts/zones/Waughroon_Shrine/mobs/Time_Bomb.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Time_Bomb.lua
@@ -2,16 +2,44 @@
 -- Area: Waughroon Shrine
 --  Mob: Time Bomb
 -- BCNM: 3, 2, 1...
+-- TODO: Players should not be expelled from the battlefield
+-- if the bomb self-destructs.
 -----------------------------------
+local ID = require("scripts/zones/Waughroon_Shrine/IDs")
+-----------------------------------
+
 local entity = {}
+
+local timeTable =
+{
+    {59, ID.text.BOMB_TIMER_1, 8},
+    {58, ID.text.BOMB_TIMER_2, 7},
+    {57, ID.text.BOMB_TIMER_3, 6},
+    {56, ID.text.BOMB_TIMER_4, 5},
+    {55, ID.text.BOMB_TIMER_5, 4},
+    {50, ID.text.BOMB_TIMER_10, 3},
+    {40, ID.text.BOMB_TIMER_20, 2},
+    {30, ID.text.BOMB_TIMER_30, 1},
+}
 
 entity.onMobEngaged = function(mob, target)
     mob:setLocalVar("selfDestruct", os.time() + 60)
+    mob:setLocalVar("warning", 8)
+    mob:messageText(target, ID.text.BOMB_TIMER_60, false)
     mob:SetAutoAttackEnabled(false)
     mob:SetMobAbilityEnabled(false)
 end
 
 entity.onMobFight = function(mob, target)
+    local warning = mob:getLocalVar("warning")
+
+    for k, v in pairs(timeTable) do
+        if warning >= k and mob:getBattleTime() == v[1] then
+            mob:messageText(target, v[2], false)
+            mob:setLocalVar("warning", warning - 1)
+        end
+    end
+
     if os.time() > mob:getLocalVar("selfDestruct") then
         mob:useMobAbility(256) -- self-destruct_321
     end
@@ -19,7 +47,7 @@ end
 
 entity.onMobWeaponSkill = function(target, mob, skill)
     if skill:getID() == 256 then
-        mob:timer(1000, function(mobArg)
+        mob:timer(5000, function(mobArg)
             mobArg:getBattlefield():lose()
         end)
     end
