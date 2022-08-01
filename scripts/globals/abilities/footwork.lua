Wings-XI/LegacyWingsXI

Footwork Update

scripts/globals/abilities/footwork.lua
@@ -3,7 +3,7 @@
 -- Makes kicks your primary mode of attack.
 -- Obtained: Monk Level 65
 -- Recast Time: 5:00
--- Duration: 1:00
+-- Duration: 5:00
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
@@ -13,7 +13,5 @@ function onAbilityCheck(player, target, ability)
 end
 
 function onUseAbility(player, target, ability)
-    local kickAttPercent = 15
-    --local kickAttPercent = 10
-    player:addStatusEffect(tpz.effect.FOOTWORK, kickAttPercent, 0, 60*5)
+    player:addStatusEffect(tpz.effect.FOOTWORK, 0, 0, 60*5)
 end

scripts/globals/effects/footwork.lua
@@ -7,16 +7,12 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.KICK_ATTACK_RATE, 15)
-    --target:addMod(tpz.mod.KICK_DMG, effect:getPower())
-    --target:addMod(tpz.mod.ATTP, effect:getPower())
+    target:addMod(tpz.mod.ATTP, 10)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.KICK_ATTACK_RATE, 15)
-    --target:delMod(tpz.mod.KICK_DMG, effect:getPower())
-    --target:delMod(tpz.mod.ATTP, effect:getPower())
+    target:delMod(tpz.mod.ATTP, 10)
 end

LightOmegaDark/ASB-river

scripts/globals/abilities/footwork.lua
@@ -3,7 +3,7 @@
 -- Makes kicks your primary mode of attack.
 -- Obtained: Monk Level 65
 -- Recast Time: 5:00
--- Duration: 1:00
+-- Duration: 5:00
 -----------------------------------
 require("scripts/globals/status")
 -----------------------------------
@@ -14,9 +14,5 @@ ability_object.onAbilityCheck = function(player, target, ability)
 end
 
 ability_object.onUseAbility = function(player, target, ability)
-    local kickDmg = 20 + player:getWeaponDmg()
-    local kickAttPercent = 25 + player:getMod(xi.mod.FOOTWORK_ATT_BONUS)
-    player:addStatusEffect(xi.effect.FOOTWORK, kickDmg, 0, 60, 0, kickAttPercent)
+    player:addStatusEffect(xi.effect.FOOTWORK, 0, 0, 60*5)
 end
-
-return ability_object

scripts/globals/effects/footwork.lua
@@ -7,20 +7,12 @@ require("scripts/globals/status")
 local effect_object = {}
 
 effect_object.onEffectGain = function(target, effect)
-    local jpLevel = target:getJobPointLevel(xi.jp.FOOTWORK_EFFECT)
-
-    target:addMod(xi.mod.KICK_ATTACK_RATE, 20)
-    target:addMod(xi.mod.KICK_DMG, effect:getPower() + jpLevel)
+    target:addMod(xi.mod.ATTP, 10)
 end
 
 effect_object.onEffectTick = function(target, effect)
 end
 
 effect_object.onEffectLose = function(target, effect)
-    local jpLevel = target:getJobPointLevel(xi.jp.FOOTWORK_EFFECT)
-
-    target:delMod(xi.mod.KICK_ATTACK_RATE, 20)
-    target:delMod(xi.mod.KICK_DMG, effect:getPower() + jpLevel)
+    target:delMod(xi.mod.ATTP, 10)
 end
-
-return effect_object