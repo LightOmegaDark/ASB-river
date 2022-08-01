Wings-XI/LegacyWingsXI

Remove third eye on final anticipate

src/map/attack.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 ===========================================================================
 
   Copyright (c) 2010-2015 Darkstar Dev Teams
@@ -347,50 +347,46 @@ bool CAttack::CheckAnticipated()
 
     //power stores how many times this effect has anticipated
     auto pastAnticipations = effect->GetPower();
-
-    if (pastAnticipations > 7)
+    if (pastAnticipations >= 6)
     {
         //max 7 anticipates!
         m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
-        return false;
+        m_anticipated = true;
+        return true;
     }
 
     bool hasSeigan = m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_SEIGAN, 0);
 
-    if (!hasSeigan && pastAnticipations == 0)
+    if (!hasSeigan)
     {
         m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
         m_anticipated = true;
         return true;
     }
-    else if (!hasSeigan)
-    {
-        m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
-        return false;
-    }
     else
-    { //do have seigan, decay anticipations correctly (guesstimated)
+    {//do have seigan, decay anticipations correctly (guesstimated)
         //5-6 anticipates is a 'lucky' streak, going to assume 15% decay per proc, with a 100% base w/ Seigan
-        if (tpzrand::GetRandomNumber(100) < (100 - (pastAnticipations * 15) + m_victim->getMod(Mod::THIRD_EYE_ANTICIPATE_RATE)))
+        if (tpzrand::GetRandomNumber(100) >= (100 - ((pastAnticipations+1) * 15) + m_victim->getMod(Mod::THIRD_EYE_ANTICIPATE_RATE)))
         {
-            //increment power and don't remove
-            effect->SetPower(effect->GetPower() + 1);
-            //chance to counter - 25% base
-            if (tpzrand::GetRandomNumber(100) < 25 + m_victim->getMod(Mod::THIRD_EYE_COUNTER_RATE))
-            {
-
-                if (m_victim->PAI->IsEngaged() && facing(m_victim->loc.p, m_attacker->loc.p, 40) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_SLEEP) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_LULLABY) &&
-                    !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_PETRIFICATION) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_TERROR))
-                {
-                    m_isCountered = true;
-                    m_isCritical = (tpzrand::GetRandomNumber(100) < battleutils::GetCritHitRate(m_victim, m_attacker, false));
-                }
-            }
-            m_anticipated = true;
-            return true;
+            m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
         }
-        m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
-        return false;
+        
+        //increment power and don't remove
+        effect->SetPower(effect->GetPower() + 1);
+        //chance to counter - 25% base
+        if (tpzrand::GetRandomNumber(100) < 25 + m_victim->getMod(Mod::THIRD_EYE_COUNTER_RATE))
+        {
+
+            if (m_victim->PAI->IsEngaged() && facing(m_victim->loc.p, m_attacker->loc.p, 40) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_SLEEP) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_LULLABY) &&
+                !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_PETRIFICATION) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_TERROR))
+            {
+                m_isCountered = true;
+                m_isCritical = (tpzrand::GetRandomNumber(100) < battleutils::GetCritHitRate(m_victim, m_attacker, false));
+            }
+        }
+
+        m_anticipated = true;
+        return true;
     }
 
     return false;

LightOmegaDark/ASB-river

src/map/attack.cpp
@@ -346,48 +346,46 @@ bool CAttack::CheckAnticipated()
 
     // power stores how many times this effect has anticipated
     auto pastAnticipations = effect->GetPower();
-
-    if (pastAnticipations > 7)
+    if (pastAnticipations >= 6)
     {
         // max 7 anticipates!
         m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
-        return false;
+        m_anticipated = true;
+        return true;
     }
 
     bool hasSeigan = m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_SEIGAN, 0);
 
-    if (!hasSeigan && pastAnticipations == 0)
+    if (!hasSeigan)
     {
         m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
         m_anticipated = true;
         return true;
     }
-    else if (!hasSeigan)
-    {
-        m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
-        return false;
-    }
     else
-    { // do have seigan, decay anticipations correctly (guesstimated)
-        // 5-6 anticipates is a 'lucky' streak, going to assume 15% decay per proc, with a 100% base w/ Seigan
-        if (xirand::GetRandomNumber(100) < (100 - (pastAnticipations * 15) + m_victim->getMod(Mod::THIRD_EYE_ANTICIPATE_RATE)))
+    {//do have seigan, decay anticipations correctly (guesstimated)
+        //5-6 anticipates is a 'lucky' streak, going to assume 15% decay per proc, with a 100% base w/ Seigan
+        if (xirand::GetRandomNumber(100) >= (100 - ((pastAnticipations+1) * 15) + m_victim->getMod(Mod::THIRD_EYE_ANTICIPATE_RATE)))
         {
-            // increment power and don't remove
-            effect->SetPower(effect->GetPower() + 1);
-            // chance to counter - 25% base
-            if (xirand::GetRandomNumber(100) < 25 + m_victim->getMod(Mod::THIRD_EYE_COUNTER_RATE))
-            {
-                if (m_victim->PAI->IsEngaged())
-                {
-                    m_isCountered = true;
-                    m_isCritical  = (xirand::GetRandomNumber(100) < battleutils::GetCritHitRate(m_victim, m_attacker, false));
-                }
-            }
-            m_anticipated = true;
-            return true;
+            m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
         }
-        m_victim->StatusEffectContainer->DelStatusEffect(EFFECT_THIRD_EYE);
-        return false;
+
+        //increment power and don't remove
+        effect->SetPower(effect->GetPower() + 1);
+        //chance to counter - 25% base
+        if (xirand::GetRandomNumber(100) < 25 + m_victim->getMod(Mod::THIRD_EYE_COUNTER_RATE))
+        {
+
+            if (m_victim->PAI->IsEngaged() && facing(m_victim->loc.p, m_attacker->loc.p, 40) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_SLEEP) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_LULLABY) &&
+                !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_PETRIFICATION) && !m_victim->StatusEffectContainer->HasStatusEffect(EFFECT_TERROR))
+            {
+                m_isCountered = true;
+                m_isCritical = (xirand::GetRandomNumber(100) < battleutils::GetCritHitRate(m_victim, m_attacker, false));
+            }
+        }
+
+        m_anticipated = true;
+        return true;
     }
     // return false;
 }