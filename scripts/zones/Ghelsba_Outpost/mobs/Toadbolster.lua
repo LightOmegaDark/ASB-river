267cf287aafb96f7bf936e8e46598b0b4e72579e
dallano <atwood.dallan@hotmail.com>
2022-08-02 00:58:21 -0600

    Toadal Recall Implementation
    Toadal Recall:
    Evidence:
    https://ffxiclopedia.fandom.com/wiki/Toadal_Recall
    https://www.youtube.com/watch?v=Uqx0tM3-OaQ&t=92s&ab_channel=Dashknocker
    https://www.youtube.com/watch?v=lOK2a4Pg5uc&t=79s&ab_channel=Dashknocker
    
    Changes:
    Toads will now use their spore moves in 5 - 20 second intervals when the target is in range as seen in the video
    After a toad has spent all shroom abilities, a timer will be set for 1-2 minutes. Once up a dust cloud effect will appear and their abilities will be recharged
    Changed all shroom.luas to modify and react according to the mobs animation sub. Which behaved similarily to the VAR that was previously being used.
    Gave toads immunity to all forms of sleep (dark and light)
    Added BCNM mob type to all toads

--- scripts/zones/Ghelsba_Outpost/mobs/Toadbolster.lua
+++ scripts/zones/Ghelsba_Outpost/mobs/Toadbolster.lua
@@ -1,16 +1,53 @@
 -----------------------------------
 -- Area: Ghelsba Outpost
---  Mob: Toadbolster
+-- Mob: Toadbolster
 -- BCNM: Toadal Recall
 -----------------------------------
 local entity = {}
 
+local shroomAbilities =
+{
+    {310, 0}, -- queasyshroom
+    {311, 1}, -- numbshroom
+    {312, 2}, -- shakeshroom
+}
+
+local function resetTimer(mob)
+    mob:setLocalVar("timer", os.time() + math.random(5,20))
+end
+
 entity.onMobSpawn = function(mob)
+    mob:setLocalVar("control", 0)
     mob:setMobMod(xi.mobMod.NO_MOVE, 1)
 end
 
 entity.onMobEngaged = function(mob)
     mob:setMobMod(xi.mobMod.NO_MOVE, 0)
+    resetTimer(mob)
+end
+
+entity.onMobFight = function(mob, target)
+    if mob:getLocalVar("control") == 0 and mob:getAnimationSub() == 3 then
+        mob:setLocalVar("control", 1)
+        mob:timer(1000 * math.random(60,120), function(mobArg)
+            mobArg:useMobAbility(626)
+            mobArg:setLocalVar("control", 0)
+            mobArg:setAnimationSub(0)
+            resetTimer(mob)
+        end)
+    end
+
+    if mob:getLocalVar("timer") < os.time() and mob:getAnimationSub() ~= 3 then
+        for _, v in pairs(shroomAbilities) do
+            if mob:getAnimationSub() == v[2] then
+                mob:queue(0, function(mobArg)
+                    mobArg:useMobAbility(v[1])
+                    resetTimer(mobArg)
+                end)
+                break
+            end
+        end
+    end
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
