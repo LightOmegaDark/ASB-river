8298fefcc1b6ba56dcc724ebc77c669995fec996
dallano <atwood.dallan@hotmail.com>
2022-08-01 23:06:27 -0600

    Early Work - stashing

--- scripts/zones/Ghelsba_Outpost/mobs/Toadpillow.lua
+++ scripts/zones/Ghelsba_Outpost/mobs/Toadpillow.lua
@@ -5,6 +5,14 @@
 -----------------------------------
 local entity = {}
 
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 1)
+end
+
+entity.onMobEngaged = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 0)
+end
+
 entity.onMobDeath = function(mob, player, isKiller)
 end
 
