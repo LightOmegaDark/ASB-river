821e1cde5aff710d94e155b72c743b3a2656a670
Abdiah <62350957+dallano@users.noreply.github.com>
2022-08-11 10:53:36 -0600

    Bugard in the Clouds + Beloved of the Atlantes implementation (#283)
    * Bugard in the Clouds
    
    Evidence:
    https://rumble.com/vbt5hx-enm-50-bugard-in-the-clouds-w-pld-rdm-smn.html
    https://ffxiclopedia.fandom.com/wiki/Bugard_in_the_Clouds
    https://www.youtube.com/watch?v=2i0qb0q-weM&ab_channel=SpielmansMog
    
    Speculations:
    used invincible at 50%, mighty strikes at 25%
    Invincible lasted the entire fight after usage
    After invincible was used, Hotupuku had 100% double attack rate
    Didn't receive enstone until exactly 30% HP
    Enstone only present on 50% of the attacks
    Hotupuku never become immune to magical damage (ranged damage undetermined) nor did it ever use hundred fists.
    Hotupuku only tripled his damaging mob skills
    
    Changes:
    Added the ability for Hotupuku to use abilities 3 times in a row
    Added enstone to come into effect under 30% HP
    Added 100% double attack after invincible is used
    Added use of mighty strikes at 25%
    Added a special case in invinicble.lua for the duration to be 15 minutes when used by Hotupuku (maximum length of battlefield)
    
    * Beloved of the Altantes Implementation
    
    Evidence:
    https://www.youtube.com/watch?v=P4Ihyucdxgc&ab_channel=NyelloCaptures
    https://ffxiclopedia.fandom.com/wiki/Beloved_of_the_Atlantes
    
    Soeculations:
    Only one guard hippogryph would come at at the 25% hp%  intervals. no more would spawn if the original is still spawned.
    Guard hippogryph would cast bard songs
    
    Changes:
    Guard Hippogryph will spawn every 25% HP watch hippogryph loses if not already spawned
    Added stun effect to watch's auto attacks
    Added drain effect to guard's auto attacks
    Implemented loot tables
    Fixed spawn points
    added BCNM + NM mob type to mob in this battlefield
    
    * Spawn point fix
    
    * Update mob_spawn_points.sql
    
    * Update mob_spawn_points.sql
    
    a fool of myself

--- scripts/globals/mobskills/invincible.lua
+++ scripts/globals/mobskills/invincible.lua
@@ -11,8 +11,15 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
     return 0
 end
 
-mobskillObject.onMobWeaponSkill = function(target, mob, skill)
-    xi.mobskills.mobBuffMove(mob, xi.effect.INVINCIBLE, 1, 0, 30)
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local duration = 30
+
+    -- Hotupuku ENM: Bugard in the Clouds
+    if mob:getPool() == 1992 then
+        duration = 900
+    end
+
+    xi.mobskills.mobBuffMove(mob, xi.effect.INVINCIBLE, 1, 0, duration)
 
     skill:setMsg(xi.msg.basic.USES)
 
