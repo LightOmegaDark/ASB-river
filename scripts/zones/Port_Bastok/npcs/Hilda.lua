Wings-XI/LegacyWingsXI

Missed NPCs for Bastok Movement Update

scripts/zones/Port_Bastok/npcs/Hilda.lua
@@ -10,7 +10,95 @@ require("scripts/globals/keyitems")
 require("scripts/globals/missions")
 require("scripts/globals/quests")
 require("scripts/globals/utils")
+require("scripts/globals/pathfind")
 -----------------------------------
+local flags = tpz.path.flag.NONE
+local path =
+{
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.574, -11.380, 18.439,
+    -157.387, -11.380, 15.589,
+    -158.694, -11.047, 15.911,
+    -160.021, -10.313, 16.424,
+    -160.902, -10.188, 16.373,
+    -162.387, -9.683, 15.364,
+    -166.233, -8.705, 13.941,
+    -162.568, -8.081, 12.267,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -166.233, -8.705, 13.941,
+    -162.387, -9.683, 15.364,
+    -160.902, -10.188, 16.373,
+    -160.021, -10.313, 16.424,
+    -158.694, -11.047, 15.911,
+    -157.387, -11.380, 15.589,
+    -155.574, -11.380, 18.439,
+    -155.302, -11.381, 18.639,
+}
+
+function onSpawn(npc)
+    npc:initNpcAi()
+    npc:setPos(tpz.path.first(path))
+    onPath(npc)
+end
+
+function onPath(npc)
+    tpz.path.patrolsimple(npc, path, flags)
+end
 
 function onTrade(player, npc, trade)
 

LightOmegaDark/ASB-river

scripts/zones/Port_Bastok/npcs/Hilda.lua
@@ -10,8 +10,95 @@ require("scripts/globals/keyitems")
 require("scripts/globals/missions")
 require("scripts/globals/quests")
 require("scripts/globals/utils")
+require("scripts/globals/pathfind")
 -----------------------------------
 local entity = {}
+
+local path =
+{
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.438, -11.380, 18.663,
+    -155.574, -11.380, 18.439,
+    -157.387, -11.380, 15.589,
+    -158.694, -11.047, 15.911,
+    -160.021, -10.313, 16.424,
+    -160.902, -10.188, 16.373,
+    -162.387, -9.683, 15.364,
+    -166.233, -8.705, 13.941,
+    -162.568, -8.081, 12.267,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -162.469, -8.081, 12.366,
+    -166.233, -8.705, 13.941,
+    -162.387, -9.683, 15.364,
+    -160.902, -10.188, 16.373,
+    -160.021, -10.313, 16.424,
+    -158.694, -11.047, 15.911,
+    -157.387, -11.380, 15.589,
+    -155.574, -11.380, 18.439,
+    -155.302, -11.381, 18.639,
+}
+
+entity.onSpawn = function(npc)
+    npc:initNpcAi()
+    npc:setPos(xi.path.first(path))
+end
+
+entity.onPath = function(npc)
+    xi.path.patrol(npc, path)
+end
 
 entity.onTrade = function(player, npc, trade)
     if trade:getGil() == 0 and trade:getItemCount() == 1 then
@@ -23,8 +110,7 @@ entity.onTrade = function(player, npc, trade)
     end
 end
 
-entity.onTrigger = function(player, npc)
-    local wildcatBastok = player:getCharVar("WildcatBastok")
+end
 
     if player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.LURE_OF_THE_WILDCAT) == QUEST_ACCEPTED and not utils.mask.getBit(wildcatBastok, 3) then
         player:startEvent(356)