8a458a9aac1f2b82daf2b4b287418561e9b94c78
Critical <lyardeen@gmail.com>
2022-08-20 23:52:22 -0600

    Adds content tag restrictions to synth recipes

--- src/map/utils/synthutils.cpp
+++ src/map/utils/synthutils.cpp
@@ -26,6 +26,8 @@
 #include <cmath>
 #include <cstring>
 
+#include "../lua/luautils.h"
+
 #include "../packets/char_skills.h"
 #include "../packets/char_update.h"
 #include "../packets/inventory_assign.h"
@@ -64,8 +66,8 @@ namespace synthutils
     {
         const char* fmtQuery =
 
-            "SELECT ID, KeyItem, Wood, Smith, Gold, Cloth, Leather, Bone, Alchemy, Cook, \
-            Result, ResultHQ1, ResultHQ2, ResultHQ3, ResultQty, ResultHQ1Qty, ResultHQ2Qty, ResultHQ3Qty, Desynth \
+            "SELECT ID, KeyItem, Wood, Smith, Gold, Cloth, Leather, Bone, Alchemy, Cook, Result, ResultHQ1, \
+            ResultHQ2, ResultHQ3, ResultQty, ResultHQ1Qty, ResultHQ2Qty, ResultHQ3Qty, Desynth, ContentTag \
         FROM synth_recipes \
         WHERE (Crystal = %u OR HQCrystal = %u) \
             AND Ingredient1 = %u \
@@ -99,6 +101,14 @@ namespace synthutils
 
                 uint16 skillValue   = 0;
                 uint16 currentSkill = 0;
+
+                const char* contentTag = (const char*)sql->GetData(19);
+
+                if (!luautils::IsContentEnabled(contentTag))
+                {
+                    PChar->pushPacket(new CSynthMessagePacket(PChar, SYNTH_BADRECIPE));
+                    return false;
+                }
 
                 for (uint8 skillID = SKILL_WOODWORKING; skillID <= SKILL_COOKING; ++skillID) // range for all 8 synth skills
                 {
