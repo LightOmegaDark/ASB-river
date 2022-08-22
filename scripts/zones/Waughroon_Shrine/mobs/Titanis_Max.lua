75dd378bbf669d1ed361fd2ed333de437514b9b7
dallano <atwood.dallan@hotmail.com>
2022-08-21 22:32:46 -0600

    Power up on death fix
    Did live capture to determine that the only mod being added to mob was delay change.

--- scripts/zones/Waughroon_Shrine/mobs/Titanis_Max.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Titanis_Max.lua
@@ -8,33 +8,13 @@ require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
-local function powerUp(mob)
-    mob:addMod(xi.mod.DELAY, -250)
-    mob:addMod(xi.mod.SLEEPRES, 50)
-    mob:addMod(xi.mod.BINDRES, 50)
-    mob:addMod(xi.mod.LULLABYRES, 50)
-    mob:addMod(xi.mod.ATT, 50)
-    mob:addMod(xi.mod.DEF, 50)
-    mob:addMod(xi.mod.MDEF, 25)
-    mob:addMod(xi.mod.MEVA, 25)
-    mob:addMod(xi.mod.EVA, 25)
-    mob:addMod(xi.mod.ACC, 25)
-end
-
 entity.onMobSpawn = function(mob)
-    xi.mix.jobSpecial.config(mob, {
-        specials =
-        {
-            {id = xi.jsa.SOUL_VOICE, cooldown = 200, hpp = 95},
-        },
-    })
-
     mob:addListener("TAKE_DAMAGE", "TITANIS_TAKE_DAMAGE", function(mobArg, amount, attacker, attackType, damageType)
         if amount > mobArg:getHP() then
             local id = mob:getID()
 
             for i = 1, 3 do
-                powerUp(GetMobByID(id + i))
+                GetMobByID(id + i):addMod(xi.mod.DELAY, 1000)
             end
         end
     end)
