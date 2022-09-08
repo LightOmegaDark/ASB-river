5bd1cff35930b4c1a504e0cbd9a53e2d0ded04ff
paladindamarus <105614059+paladindamarus@users.noreply.github.com>
2022-09-08 08:20:17 -0400

    Added new line for sanity checks

--- scripts/globals/mobskills/ultimate_terror.lua
+++ scripts/globals/mobskills/ultimate_terror.lua
@@ -19,6 +19,18 @@ mobskill_object.onMobWeaponSkill = function(target, mob, skill)
     local drained = 0
     local threshold = 3/7
 
+    local effects = 
+    {
+        {xi.effect.STR_DOWN, 10, 3, 60},
+        {xi.effect.DEX_DOWN, 10, 3, 60},
+        {xi.effect.VIT_DOWN, 10, 3, 60},
+        {xi.effect.AGI_DOWN, 10, 3, 60},
+        {xi.effect.INT_DOWN, 10, 3, 60},
+        {xi.effect.MND_DOWN, 10, 3, 60},
+        {xi.effect.CHR_DOWN, 10, 3, 60}
+    }
+
+    for _, eff in pairs(effects) do
     if math.random() < threshold then
         skill:setMsg(xi.mobskills.mobDrainAttribute(mob, target, xi.effect.STR_DOWN, 10, 3, 60))
         drained = drained + 1
