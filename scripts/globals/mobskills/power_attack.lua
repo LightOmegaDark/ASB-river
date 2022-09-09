35ebaf6291bcaf3dd46110e3a9e8491b2484db7a
Critical <lyardeen@gmail.com>
2022-09-09 00:41:26 -0600

    Set correct blade bash animation based on weapon; adjusted Overgrown Ivy mechanics; added functionality to talisman obi Co-Authored by: Zelita <siknoz@yahoo.com

--- scripts/globals/mobskills/power_attack.lua
+++ scripts/globals/mobskills/power_attack.lua
@@ -9,8 +9,9 @@ require("scripts/globals/mobskills")
 -----------------------------------
 local mobskillObject = {}
 
-mobskillObject.onMobSkillCheck = function(target, mob, skill)
-    if mob:getMainJob() ~= xi.job.WAR then
+mobskill_object.onMobSkillCheck = function(target, mob, skill)
+    local mobSkin = mob:getModelId() --Mobskill based on modelid, this is for all h2h models
+    if mobSkin == 271 or mobSkin == 642 or mobSkin == 643 or mobSkin == 709 or mobSkin == 711 then
         return 0
     end
 
