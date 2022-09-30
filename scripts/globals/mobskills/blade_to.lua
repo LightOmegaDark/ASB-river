8be4d5956964b1e735d1658a4f5cc1e07a53eb69
dallano <atwood.dallan@hotmail.com>
2022-09-30 11:44:04 -0600

    Weaponskill Balancing

--- scripts/globals/mobskills/blade_to.lua
+++ scripts/globals/mobskills/blade_to.lua
@@ -13,10 +13,10 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
     return 0
 end
 
-mobskillObject.onMobWeaponSkill = function(target, mob, skill)
-    local numhits = 3
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local numhits = 1
     local accmod = 1
-    local dmgmod = 2
+    local dmgmod = 1.8
     local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.ACC_VARIES, 1, 2, 3)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
 
--- scripts/globals/mobskills/raiden_thrust.lua
+++ scripts/globals/mobskills/raiden_thrust.lua
@@ -1,9 +1,6 @@
 -----------------------------------
 -- Raiden Thrust
---
 -- Deals lightning elemental damage. Damage varies with TP.
--- Type: Physical
--- Utsusemi/Blink absorb: 1 Shadow
 -- Range: Melee
 -----------------------------------
 require("scripts/globals/mobskills")
@@ -17,11 +14,10 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
     return 0
 end
 
-mobskillObject.onMobWeaponSkill = function(target, mob, skill)
-    local dmgmod = 1.25
-
-    local info = xi.mobskills.mobMagicalMove(mob, target, skill, mob:getWeaponDmg() * 4, xi.magic.ele.THUNDER, dmgmod, xi.mobskills.magicalTpBonus.DMG_BONUS, 2)
-    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.MAGICAL, xi.damageType.THUNDER, xi.mobskills.shadowBehavior.NUMSHADOWS_1)
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local dmgmod = 1.8
+    local info = xi.mobskills.mobMagicalMove(mob, target, skill, mob:getWeaponDmg()*3, xi.magic.ele.THUNDER, dmgmod, xi.mobskills.magicalTpBonus.DMG_BONUS, 1)
+    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.MAGICAL, xi.damageType.THUNDER, info.hitslanded)
 
     target:takeDamage(dmg, mob, xi.attackType.MAGICAL, xi.damageType.THUNDER)
     return dmg
