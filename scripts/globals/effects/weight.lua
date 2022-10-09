174ccf00c25176d05d07aea61a35a361b2af191a
OpheliaXI <heliashelium@gmail.com>
2022-10-09 13:24:59 -0500

    Fix Gravity Speed Setting

--- scripts/globals/effects/weight.lua
+++ scripts/globals/effects/weight.lua
@@ -5,16 +5,16 @@ require("scripts/globals/status")
 -----------------------------------
 local effectObject = {}
 
-effectObject.onEffectGain = function(target, effect)
-    target:addMod(xi.mod.MOVE, -effect:getPower())
+effect_object.onEffectGain = function(target, effect)
+    target:setSpeed(target:getSpeed() - effect:getPower())
     target:addMod(xi.mod.EVA, -10)
 end
 
 effectObject.onEffectTick = function(target, effect)
 end
 
-effectObject.onEffectLose = function(target, effect)
-    target:delMod(xi.mod.MOVE, -effect:getPower())
+effect_object.onEffectLose = function(target, effect)
+    target:setSpeed(target:getSpeed() + effect:getPower())
     target:delMod(xi.mod.EVA, -10)
 end
 
