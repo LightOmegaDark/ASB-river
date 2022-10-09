70584883bb46fe0a3e21c24e5c635cc6164729c9
dallano <atwood.dallan@hotmail.com>
2022-10-09 17:37:56 -0600

    Update charentity.cpp
    Fixes: https://github.com/HorizonFFXI/HorizonXI-Issues/issues/181

--- src/map/entities/charentity.cpp
+++ src/map/entities/charentity.cpp
@@ -2239,7 +2239,7 @@ void CCharEntity::Die()
 
     battleutils::RelinquishClaim(this);
 
-    if (this->PPet)
+    if (this->PPet && this->objtype == TYPE_PC)
     {
         if (PPet->StatusEffectContainer->HasStatusEffect(EFFECT_CHARM))
         {
