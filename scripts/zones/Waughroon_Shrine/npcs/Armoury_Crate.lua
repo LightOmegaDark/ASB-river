a4f29f3861b5975a17761289666ec82fdaef26ae
dallano <atwood.dallan@hotmail.com>
2022-10-24 22:54:52 -0600

    General BCNM Fixes
    Prepares grips to be removed from CoP era in horizon
    
    Fixes: https://github.com/HorizonFFXI/HorizonXI-Issues/issues/278
    Fixes: https://github.com/HorizonFFXI/HorizonXI-Issues/issues/252

--- scripts/zones/Waughroon_Shrine/npcs/Armoury_Crate.lua
+++ scripts/zones/Waughroon_Shrine/npcs/Armoury_Crate.lua
@@ -289,13 +289,17 @@ local loot =
     [76] =
     {
         {
-            {itemid = 18047, droprate =  188}, -- Havoc Scythe
-            {itemid = 17939, droprate =   27}, -- Kriegsbeil
-            {itemid = 17937, droprate =  170}, -- Leopard Axe
-            {itemid =  1441, droprate =  295}, -- Libation Abjuration
-            {itemid = 18351, droprate =   27}, -- Meteor Cesti
-            {itemid = 17464, droprate =   71}, -- Purgatory Mace
-            {itemid = 17575, droprate =  196}, -- Somnus Signa
+                { itemid = 3343, droprate = 1000 }, -- Blue Pondweed
+        },
+
+        {
+            { itemid = 18047, droprate = 188 }, -- Havoc Scythe
+            { itemid = 17939, droprate =  27 }, -- Kriegsbeil
+            { itemid = 17937, droprate = 170 }, -- Leopard Axe
+            { itemid =  1441, droprate = 295 }, -- Libation Abjuration
+            { itemid = 18351, droprate =  27 }, -- Meteor Cesti
+            { itemid = 17464, droprate =  71 }, -- Purgatory Mace
+            { itemid = 17575, droprate = 196 }, -- Somnus Signa
         },
 
         {
