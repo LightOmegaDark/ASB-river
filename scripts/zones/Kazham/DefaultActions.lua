73646c81b7e92f68084b4b8b4faeaf9c97f58121
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-11-17 20:11:14 -0500

    NPC Fixes | LB2 2hr fix

--- scripts/zones/Kazham/DefaultActions.lua
+++ scripts/zones/Kazham/DefaultActions.lua
@@ -5,4 +5,10 @@ return {
     ['Hari_Pakhroib']    = { event = 84 },
     ['Jakoh_Wahcondalo'] = { event = 113 },
     ['Vanono']           = { event = 262 },
+    ['Lulupp']           = { text = ID.text.OPOOPO_DIALOG },
+    ['Tatapp']           = { text = ID.text.OPOOPO_DIALOG },
+    ['Roropp']           = { text = ID.text.OPOOPO_DIALOG },
+    ['Kukupp']           = { text = ID.text.OPOOPO_DIALOG },
+    ['Bubupp']           = { text = ID.text.OPOOPO_DIALOG },
+    ['Popopp']           = { text = ID.text.OPOOPO_DIALOG },
 }
