eabd61dd0e8118223b0dd7ea7c6c2b4dfb53d633
OpheliaXI <heliashelium@gmail.com>
2022-11-26 22:45:19 -0600

    Fix Hydra NMs Looks and Status

--- src/map/lua/lua_zone.cpp
+++ src/map/lua/lua_zone.cpp
@@ -309,7 +309,7 @@ std::optional<CLuaBaseEntity> CLuaZone::insertDynamicEntity(sol::table table)
 
         PMob->m_isAggroable = table["isAggroable"].get_or(false);
 
-        PMob->spawnAnimation = static_cast<SPAWN_ANIMATION>(table["specialSpawnAnimation"].get_or(false) ? 1 : 0);
+        PMob->spawnAnimation = table["specialSpawnAnimation"].get_or(false) ? SPAWN_ANIMATION::SPECIAL : SPAWN_ANIMATION::NORMAL;
 
         PMob->spawnAnimation = static_cast<SPAWN_ANIMATION>(table["specialSpawnAnimation"].get_or(false) ? 1 : 0);
 
@@ -343,7 +343,7 @@ std::optional<CLuaBaseEntity> CLuaZone::insertDynamicEntity(sol::table table)
         std::memcpy(&PEntity->look, &look, sizeof(PEntity->look));
     }
 
-    PEntity->updatemask |= UPDATE_ALL_CHAR | UPDATE_ALL_MOB | UPDATE_LOOK | UPDATE_STATUS | UPDATE_HP;
+    PEntity->updatemask |= UPDATE_ALL_CHAR;
 
     return CLuaBaseEntity(PEntity);
 }
