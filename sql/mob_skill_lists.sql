88320245031aa5eaca6f6e65ee0fe17ee879ccf3
jmcmorris <jasson@siegegames.com>
2022-12-04 17:50:23 -0800

    Changing Ultima in One to be Feared to use stages and skill lists

--- sql/mob_skill_lists.sql
+++ sql/mob_skill_lists.sql
@@ -2450,9 +2450,21 @@ INSERT INTO `mob_skill_lists` VALUES ('Proto-Omega',727,1539); -- rear_lasers
 INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase1',728,1259); -- wire_cutter
 INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase1',728,1269); -- chemical_bomb
 INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase1',728,1270); -- particle_shield
-INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima_Phase1',729,1260); -- antimatter
-INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima_Phase1',729,1259); -- wire_cutter
-INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima_Phase1',729,1269); -- chemical_bomb
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1259);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1260);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1261);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1262);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1263);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1264);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1265);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1266);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1267);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1268);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1269);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1521);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1522);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1523);
+INSERT INTO `mob_skill_lists` VALUES ('Proto-Ultima',729,1540);
 INSERT INTO `mob_skill_lists` VALUES ('Tiamat-Flying-Attack',730,1278);
 INSERT INTO `mob_skill_lists` VALUES ('Ouryu-Flying-Attack',731,1298);
 INSERT INTO `mob_skill_lists` VALUES ('Jormungand-Flying-Attack',732,1288);
@@ -3822,6 +3834,13 @@ INSERT INTO `mob_skill_lists` VALUES ('Proto-Omega_Standing',1188,1529); -- hype
 INSERT INTO `mob_skill_lists` VALUES ('Proto-Omega_Standing',1188,1530); -- stun_cannon
 INSERT INTO `mob_skill_lists` VALUES ('Proto-Omega_Final',1189,1526); -- colossal_blow
 INSERT INTO `mob_skill_lists` VALUES ('Proto-Omega_Final',1189,1527); -- laser_shower
+
+INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase2',1190,1268); -- nuclear_waste
+INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase3',1191,1261); -- equalizer
+INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase3',1191,1269); -- chemical_bomb
+INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase3',1191,1270); -- particle_shield
+INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase4',1192,1260); -- antimatter
+INSERT INTO `mob_skill_lists` VALUES ('Ultima_Phase4',1192,1270); -- particle_shield
 
 -- Next available ID: 1190
 
