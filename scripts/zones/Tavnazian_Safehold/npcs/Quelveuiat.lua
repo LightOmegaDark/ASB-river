576201d4cad78349afa7b938ceeb69c13ab412ab
Scarifi3dXI <112238268+Scarifi3dXI@users.noreply.github.com>
2022-12-05 21:57:44 -0500

    Lucky Roll Npcs Implementation and more
    Adds logic to Lucky Roll Npcs Mileon in Rabao, Repat in Norg. Also a quick update to starlight_celebrations to include dream mittens within reward tables.

--- scripts/zones/Tavnazian_Safehold/npcs/Quelveuiat.lua
+++ scripts/zones/Tavnazian_Safehold/npcs/Quelveuiat.lua
@@ -10,6 +10,18 @@ local ID = require("scripts/zones/Tavnazian_Safehold/IDs")
 -----------------------------------
 local entity = {}
 
+local pathNodes =
+{
+    { x = -3.177, y = -22.750, z = -25.970, rotation = 132, wait = 35000 },
+    { x = 3.177, y = -22.750, z = -25.970, rotation = 0, wait = 35000 },
+}
+
+entity.onSpawn = function(npc)
+    npc:initNpcAi()
+    npc:setPos(xi.path.first(pathNodes))
+    npc:pathThrough(pathNodes, xi.path.flag.PATROL)
+end
+
 entity.onTrade = function(player, npc, trade)
 end
 
