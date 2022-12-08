01d29bc291c88332751d55cbf98a0d747894a374
OpheliaXI <heliashelium@gmail.com>
2022-12-07 22:28:16 -0600

    More Magic Hit Rate Work For New Findings | Fix Elemental DOTs

--- scripts/globals/spells/enhancing_spell.lua
+++ scripts/globals/spells/enhancing_spell.lua
@@ -205,16 +205,12 @@ xi.spells.enhancing.calculateEnhancingBasePower = function(caster, target, spell
 
         basePower = utils.clamp(basePower, 40, 150) -- Max is 150 and min is 40 at skill 0.
     -- Bar-Status
-    elseif spellEffect == xi.effect.BARAMNESIA or (spellEffect >= xi.effect.BARSLEEP and spellEffect <= xi.effect.BARVIRUS) then
-        -- Works the same as Bar-Element spells. This is only out of 100
-        if skillLevel > 300 then
-            basePower = (25 + math.floor(skillLevel / 4)) / 10 -- 15 at 500
-        else
-            basePower = (40 + math.floor(skillLevel / 5)) / 10 -- 10 at 300
-        end
-
-        basePower = utils.clamp(basePower, 4, 15) -- Max is 15 and min is 4 a skill 0.
-
+    elseif
+        spellEffect == xi.effect.BARAMNESIA or
+        (spellEffect >= xi.effect.BARSLEEP and
+        spellEffect <= xi.effect.BARVIRUS)
+    then
+        basePower = 20
     -- Boost-Stat / Gain-Stat
     elseif spellEffect >= xi.effect.STR_BOOST and spellEffect <= xi.effect.CHR_BOOST then
         basePower = basePower + utils.clamp(math.floor((skillLevel - 300) / 10), 0, 20)
