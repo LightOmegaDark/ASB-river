970fa07ccb94e8cc2bb9b5fd938b04aa5ee490d1
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-10 12:41:49 -0500

    Bugard Mobskills

--- scripts/globals/mobskills/scutum.lua
+++ scripts/globals/mobskills/scutum.lua
@@ -14,7 +14,7 @@ end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local typeEffect = xi.effect.DEFENSE_BOOST
-    skill:setMsg(xi.mobskills.mobBuffMove(mob, typeEffect, 30, 0, 120))
+    skill:setMsg(xi.mobskills.mobBuffMove(mob, typeEffect, 70, 0, 180))
     return typeEffect
 end
 
