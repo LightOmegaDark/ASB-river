f00e80b80d82f52746f4b8aee5ad783c72aad9b7
Scarifi3dXI <112238268+Scarifi3dXI@users.noreply.github.com>
2022-12-10 08:22:15 -0500

    Update starlight_celebrations.lua
    Remove new code until prepared to implement

--- scripts/globals/events/starlight_celebrations.lua
+++ scripts/globals/events/starlight_celebrations.lua
@@ -25,37 +25,6 @@ function xi.events.starlightCelebration.isStarlightEnabled()
     return option
 end
 
-local musicZones =
-{
-    xi.zone.UPPER_JEUNO,
-    xi.zone.LOWER_JEUNO,
-    xi.zone.PORT_JEUNO,
-}
-
-local starlightCelebrationMusic = 239
-local grandDuchyOfJeunoMusic    = 110
-
-xi.events.starlightCelebration.setMusic = function(musicId)
-    for _, zoneId in pairs(musicZones) do
-        local zone = GetZone(zoneId)
-        if zone then
-            -- TODO: Save the original music into a table
-
-            -- Set the music of the zone, so that players zoning in will
-            -- be sent it immediately
-            zone:setBackgroundMusicDay(musicId)
-            zone:setBackgroundMusicNight(musicId)
-
-            -- Set the music for players already in the zone (this is wiped when
-            -- they zone out)
-            for _, player in pairs(zone:getPlayers()) do
-                player:changeMusic(0, musicId)
-                player:changeMusic(1, musicId)
-            end
-        end
-    end
-end
-
 --------------------------------
 -- For the Children Sub-Quest --
 --------------------------------
