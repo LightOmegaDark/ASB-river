fba49436561b9e4d9f4b537e7cacc5d4fe27996e
claywar <claywar@beerlab.dev>
2022-12-11 08:41:32 -0500

    Interaction - Quest: Save My Sister
    Update quests.lua comments
    
    Fix npc ref

--- scripts/zones/Upper_Jeuno/npcs/Baudin.lua
+++ scripts/zones/Upper_Jeuno/npcs/Baudin.lua
@@ -3,11 +3,6 @@
 --  NPC: Baudin
 -- !pos -75 0 80 244
 -----------------------------------
-local ID = require("scripts/zones/Upper_Jeuno/IDs")
-require("scripts/globals/settings")
-require("scripts/globals/titles")
-require("scripts/globals/quests")
------------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
@@ -21,23 +16,8 @@ end
 
 entity.onEventFinish = function(player, csid, option)
     if csid == 177 then
-        player:addCharVar("saveTheClockTowerVar", 1)
-        player:addCharVar("saveTheClockTowerNPCz2", 32)
-    elseif csid == 105 then
-        player:setCharVar("saveMySisterVar", 3)
-    elseif csid == 107 then
-        if player:getFreeSlotsCount() == 0 then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 17041)
-        else
-            player:addTitle(xi.title.EXORCIST_IN_TRAINING)
-            player:addGil(xi.settings.main.GIL_RATE*3000)
-            player:messageSpecial(ID.text.GIL_OBTAINED, xi.settings.main.GIL_RATE*3000)
-            player:addItem(17041)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 17041)
-            player:tradeComplete()
-            player:addFame(xi.quest.fame_area.JEUNO, 30)
-            player:completeQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.SAVE_MY_SISTER)
-        end
+        player:incrementCharVar("saveTheClockTowerVar", 1)
+        player:incrementCharVar("saveTheClockTowerNPCz2", 32)
     end
 end
 
