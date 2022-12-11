eb6dd8dbc93469a118ac36b9039346b80556744a
OpheliaXI <heliashelium@gmail.com>
2022-12-10 18:46:38 -0600

    Cherry-Pick Fix trigger bindings for CLuaAction

--- src/map/ai/states/magic_state.cpp
+++ src/map/ai/states/magic_state.cpp
@@ -84,15 +84,9 @@ CMagicState::CMagicState(CBattleEntity* PEntity, uint16 targid, SpellID spellid,
     actionTarget.speceffect = SPECEFFECT::NONE;
     actionTarget.animation  = 0;
     actionTarget.param      = static_cast<uint16>(m_PSpell->getID());
-    if (m_PEntity->objtype == TYPE_MOB)
-    {
-        actionTarget.messageID = 3; // starts casting
-    }
-    else
-    {
-        actionTarget.messageID = 327; // starts casting on <target>
-    }
-    m_PEntity->PAI->EventHandler.triggerListener("MAGIC_START", CLuaBaseEntity(m_PEntity), CLuaSpell(m_PSpell.get()), &action); // TODO: weaponskill lua object
+    actionTarget.messageID  = m_PEntity->objtype == TYPE_MOB ? 3 : 327; // Mobs start casing immediately. Everything else sends a message.
+
+    m_PEntity->PAI->EventHandler.triggerListener("MAGIC_START", CLuaBaseEntity(m_PEntity), CLuaSpell(m_PSpell.get()), CLuaAction(&action));
 
     m_PEntity->loc.zone->PushPacket(m_PEntity, CHAR_INRANGE_SELF, new CActionPacket(action));
 }
