6f2736e98c3dfc2d2addf8d22e82056bb34e310c
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-11 14:55:40 -0500

    Yagudo Mobskills

--- scripts/globals/mobskills/feather_storm.lua
+++ scripts/globals/mobskills/feather_storm.lua
@@ -15,9 +15,8 @@ end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 1
-    local accmod = 2
-    local dmgmod = 1
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.DMG_VARIES, 1, 2, 3)
+    local accmod = 1
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, 1, xi.mobskills.physicalTpBonus.RANGED, 1, 2, 3)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.PIERCING, info.hitslanded)
 
     local typeEffect = xi.effect.POISON
