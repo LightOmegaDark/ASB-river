44bfa21496b2c63c2b6fc10920b1f7eeb27b94e6
Ophelia <heliashelium@gmail.com>
2022-12-11 02:28:53 -0600

    Healing Spells Lua | Healing Spell Audit (#2145)
    * Move Healing Spells to Separate Lua | Audit Healing Spells
    
    * Update scripts/globals/spells/healing_spell.lua
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>
    
    * Update scripts/globals/spells/healing_spell.lua
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>
    
    * Update scripts/globals/spells/healing_spell.lua
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>
    
    * Update scripts/globals/spells/healing_spell.lua
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>

--- scripts/globals/spells/white/curaga.lua
+++ scripts/globals/spells/white/curaga.lua
@@ -6,6 +6,7 @@ require("scripts/globals/settings")
 require("scripts/globals/status")
 require("scripts/globals/magic")
 require("scripts/globals/msg")
+require("scripts/globals/spells/healing_spell")
 -----------------------------------
 local spellObject = {}
 
@@ -14,48 +15,7 @@ spellObject.onMagicCastingCheck = function(caster, target, spell)
 end
 
 spellObject.onSpellCast = function(caster, target, spell)
-    local minCure = 60
-
-    local divisor = 1
-    local constant = 20
-    local power = xi.magic.getCurePowerOld(caster)
-    if (power > 170) then
-        divisor = 35.6666
-        constant = 87.62
-    elseif power > 110 then
-        divisor =  2
-        constant = 47.5
-    end
-
-    local final = xi.magic.getCureFinal(caster, spell, xi.magic.getBaseCureOld(power, divisor, constant), minCure, false)
-
-    final = final + (final * (target:getMod(xi.mod.CURE_POTENCY_RCVD) / 100))
-
-    --Applying server mods
-    final = final * xi.settings.main.CURE_POWER
-
-    local diff = (target:getMaxHP() - target:getHP())
-    if final > diff then
-        final = diff
-    end
-
-    target:addHP(final)
-
-    target:wakeUp()
-    caster:updateEnmityFromCure(target, final)
-
-    if target:getID() == spell:getPrimaryTargetID() then
-        spell:setMsg(xi.msg.basic.MAGIC_RECOVERS_HP)
-    else
-        spell:setMsg(xi.msg.basic.SELF_HEAL_SECONDARY)
-    end
-
-    local mpBonusPercent = (final * caster:getMod(xi.mod.CURE2MP_PERCENT)) / 100
-    if mpBonusPercent > 0 then
-        caster:addMP(mpBonusPercent)
-    end
-
-    return final
+    return xi.spells.healing.doHealingSpell(caster, target, spell, true)
 end
 
 return spellObject
