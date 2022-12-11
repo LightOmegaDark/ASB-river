581c5c0b3a5e146eceed8b4c6c592b11ac2bfc3f
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-11 18:30:07 -0500

    Ahriman Mobskills

--- scripts/globals/mobskills/hypnosis.lua
+++ scripts/globals/mobskills/hypnosis.lua
@@ -15,7 +15,7 @@ end
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local typeEffect = xi.effect.SLEEP_I
 
-    skill:setMsg(xi.mobskills.mobGazeMove(mob, target, typeEffect, 1, 0, 30))
+    skill:setMsg(xi.mobskills.mobGazeMove(mob, target, typeEffect, 1, 0, math.random(45, 60)))
 
     return typeEffect
 end
