f4f1af89e81a82e9f323433423c59133a81764e1
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-11 13:57:30 -0500

    Fomor Mobskills

--- scripts/globals/mobskills/barbed_crescent.lua
+++ scripts/globals/mobskills/barbed_crescent.lua
@@ -3,7 +3,7 @@
 -- Slashing damage varies with TP. Additional effect: Accuracy Down.
 -- Area of Effect is centered around caster.
 -- The Additional Effect: Accuracy Down may not always process.
--- Duration: Three minutes ?
+-- Duration: Two minutes
 -----------------------------------
 require("scripts/globals/mobskills")
 require("scripts/globals/settings")
@@ -17,13 +17,13 @@ end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 1
-    local accmod = 1
-    local dmgmod = 1
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.DMG_VARIES, 1, 2, 3)
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, 1, 1, xi.mobskills.physicalTpBonus.DMG_VARIES, 1.5, 2, 2.5)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
-    local typeEffect = xi.effect.ACCURACY_DOWN
 
-    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 50, 0, 120)
+    local typeEffect = xi.effect.ACCURACY_DOWN
+    local typeEffectTwo = xi.effect.BLINDNESS
+    xi.mobskills.mobStatusEffectMove(mob, target, typeEffectTwo, 15, 3, 120)
+    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, typeEffect, 100, 0, 120)
 
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
     return dmg
