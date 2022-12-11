00caf2360dcc8ecaed422b731c5f985163da41d6
claywar <claywar@beerlab.dev>
2022-12-11 14:49:07 -0500

    Interaction - Quest: The Clockmaster
    Fix logic, invalid item ref

--- scripts/zones/Upper_Jeuno/npcs/Collet.lua
+++ scripts/zones/Upper_Jeuno/npcs/Collet.lua
@@ -9,11 +9,6 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    if player:getQuestStatus(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.THE_CLOCKMASTER) == QUEST_COMPLETED then
-        player:startEvent(163)
-    else
-        player:startEvent(114)
-    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
--- scripts/zones/Upper_Jeuno/npcs/_6s2.lua
+++ scripts/zones/Upper_Jeuno/npcs/_6s2.lua
@@ -15,47 +15,6 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if csid == 118 and option == 1 then
-        player:addQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.A_CLOCK_MOST_DELICATE)
-        player:setCharVar("aClockMostdelicateVar", 0)
-
-    elseif csid == 119 then
-        if option == 0 then
-            player:setCharVar("aClockMostdelicateVar", 2)
-        elseif option == 1 then
-            player:addQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.A_CLOCK_MOST_DELICATE)
-            player:setCharVar("aClockMostdelicateVar", 0)
-        end
-
-    elseif csid == 152 then
-        if player:getFreeSlotsCount() == 0 then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 17083)
-        else
-            player:addQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.THE_CLOCKMASTER)
-            player:addTitle(xi.title.TIMEKEEPER)
-            player:addGil(1200)
-            player:messageSpecial(ID.text.GIL_OBTAINED, 1200)
-            player:addItem(17083)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 17083)
-            player:addFame(xi.quest.fame_area.JEUNO, 30)
-            player:completeQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.THE_CLOCKMASTER)
-        end
-
-    elseif csid == 202 then
-        if player:getFreeSlotsCount() == 0 then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 12727)
-        else
-            player:addTitle(xi.title.PROFESSIONAL_LOAFER)
-            player:delKeyItem(xi.ki.CLOCK_TOWER_OIL)
-            player:addGil(1200)
-            player:messageSpecial(ID.text.GIL_OBTAINED, 1200)
-            player:addItem(12727)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 12727)
-            player:addFame(xi.quest.fame_area.JEUNO, 30)
-            player:completeQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.A_CLOCK_MOST_DELICATE)
-            player:addQuest(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.SAVE_THE_CLOCK_TOWER) -- Start next quest "Save the Clock Tower"
-        end
-    end
 end
 
 return entity
