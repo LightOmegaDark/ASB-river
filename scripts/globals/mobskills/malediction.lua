121bb0a7416bf2b22cede091ff6670814553ffab
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-11 21:51:02 -0500

    Bomb/Snoll Mobskills

--- scripts/globals/mobskills/malediction.lua
+++ scripts/globals/mobskills/malediction.lua
@@ -13,7 +13,7 @@ end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local baseDmg = mob:getMainLvl() * 4
-    local info = xi.mobskills.mobMagicalMove(mob, target, skill, , xi.magic.ele.DARK, baseDmg, xi.mobskills.magicalTpBonus.MAB_BONUS, 1)
+    local info = xi.mobskills.mobMagicalMove(mob, target, skill, baseDmg, xi.magic.ele.DARK, 1, xi.mobskills.magicalTpBonus.MAB_BONUS, 1)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, MOBSKILL_MAGICAL, xi.damageType.DARK, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
 
     skill:setMsg(xi.mobskills.mobPhysicalDrainMove(mob, target, skill, xi.mobskills.drainType.HP, dmg))
