62918f4b35963598e48d85d416bdfe84af5f9467
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-13 02:08:21 -0500

    Kindred Mobskills

--- scripts/globals/mobskills/hecatomb_wave.lua
+++ scripts/globals/mobskills/hecatomb_wave.lua
@@ -19,7 +19,7 @@ end
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.BLINDNESS, 30, 0, 120)
 
-    local dmgmod = xi.mobskills.mobBreathMove(mob, target, 0.1, 1.5, xi.magic.ele.WIND, 400)
+    local dmgmod = xi.mobskills.mobBreathMove(mob, target, 0.1, 1.5, xi.magic.ele.WIND, 260)
 
     local dmg = xi.mobskills.mobFinalAdjustments(dmgmod, mob, skill, target, xi.attackType.BREATH, xi.damageType.WIND, xi.mobskills.shadowBehavior.IGNORE_SHADOWS)
     target:takeDamage(dmg, mob, xi.attackType.BREATH, xi.damageType.WIND)
--- sql/mob_skills.sql
+++ sql/mob_skills.sql
@@ -537,7 +537,7 @@ INSERT INTO `mob_skills` VALUES (635,379,'burst',0,15.0,2000,1500,4,0,0,0,0,0,0)
 INSERT INTO `mob_skills` VALUES (636,380,'flame_arrow',0,10.0,2000,1500,4,0,0,0,0,0,0);
 INSERT INTO `mob_skills` VALUES (637,381,'firebomb',6,9.5,2000,1500,4,0,0,0,0,0,0);
 INSERT INTO `mob_skills` VALUES (638,382,'blastbomb',2,13.5,2000,1500,4,0,0,0,0,0,0);
-INSERT INTO `mob_skills` VALUES (639,383,'fountain',4,9.5,2000,1500,4,0,0,0,0,0,0);
+INSERT INTO `mob_skills` VALUES (639,383,'fountain',4,9.5,2000,1500,4,0,0,1,0,0,0);
 -- INSERT INTO `mob_skills` VALUES (640,384,'touchdown',0,7.0,2000,1500,4,0,0,0,0,0,0);
 INSERT INTO `mob_skills` VALUES (641,973,'recoil_dive',4,9.5,2000,1500,4,0,0,0,0,0,0);
 INSERT INTO `mob_skills` VALUES (642,386,'flame_breath',4,12.0,2000,1500,4,0,0,0,0,0,0);
