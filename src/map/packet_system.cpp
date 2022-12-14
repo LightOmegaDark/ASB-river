46751452989e741316868ccb2295bc5b1250f724
Panic Stevenson <panic.stevenson@gmail.com>
2022-12-14 15:24:50 -0800

    Persist Player Monstrosity Data

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -331,9 +331,20 @@ void SmallPacket0x00A(map_session_data_t* const PSession, CCharEntity* const PCh
         ShowWarning("Client cannot receive packet or key is invalid: %s, Zone: %s (%i)",
                     PChar->GetName(), PChar->loc.zone->GetName(), PChar->loc.zone->GetID());
 
-        // TODO: work out how to drop player in moghouse that exits them to the zone they were in before this happened, like we used to.
-        ShowWarning("packet_system::SmallPacket0x00A dumping player `%s` to homepoint!", PChar->GetName());
-        charutils::HomePoint(PChar);
+        charutils::SaveCharPosition(PChar);
+        charutils::SaveZonesVisited(PChar);
+        charutils::SavePlayTime(PChar);
+
+        if (PChar->getCharVar("Monstrosity") != 0)
+        {
+            PChar->m_Monstrosity = PChar->getCharVar("Monstrosity");
+        }
+
+        if (PChar->m_moghouseID != 0)
+        {
+            PChar->m_charHistory.mhEntrances++;
+            gardenutils::UpdateGardening(PChar, false);
+        }
     }
 
     // Only release client from "Downloading Data" if the packet sequence came in without a drop on 0x00D
