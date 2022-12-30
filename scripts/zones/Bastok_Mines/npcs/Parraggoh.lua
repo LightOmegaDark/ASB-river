00de25c041352e1a48a7777ca00ba856e8fb27f7
Wayne Work <animagic@gmail.com>
2022-12-30 02:10:10 -0500

    2413 Talib Quest Revamp and Shady Business Quest CS fix (#2423)
    * Added Beauty_And_The_Galka.lua from another PR.
    
    * Shady_Business.lua quest CS fixed.
    
    * Parraggoh.lua and Talib.lua removed personal script quest setup
    
    * sanity fix
    
    * Moved all quest_accepted into a single entry
    
    * Sanity fix

--- scripts/zones/Bastok_Mines/npcs/Parraggoh.lua
+++ scripts/zones/Bastok_Mines/npcs/Parraggoh.lua
@@ -15,20 +15,6 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if csid == 7 and option == 0 then
-        player:addQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BEAUTY_AND_THE_GALKA)
-    elseif csid == 10 then
-        if player:getFreeSlotsCount() >= 1 then
-            player:completeQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BEAUTY_AND_THE_GALKA)
-            player:setCharVar("BeautyAndTheGalkaDenied", 0)
-            player:delKeyItem(xi.ki.PALBOROUGH_MINES_LOGS)
-            player:addFame(xi.quest.fame_area.BASTOK, 75)
-            player:addItem(16465)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 16465)
-        else
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 16465)
-        end
-    end
 end
 
 return entity
--- scripts/zones/Port_Bastok/npcs/Talib.lua
+++ scripts/zones/Port_Bastok/npcs/Talib.lua
@@ -4,66 +4,18 @@
 -- Starts Quest: Beauty and the Galka
 -- Starts & Finishes Quest: Shady Business
 -----------------------------------
-require("scripts/globals/settings")
-require("scripts/globals/keyitems")
-require("scripts/globals/shop")
-require("scripts/globals/quests")
-local ID = require("scripts/zones/Port_Bastok/IDs")
------------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local beautyAndTheGalka = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BEAUTY_AND_THE_GALKA)
-    local shadyBusiness = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.SHADY_BUSINESS)
-
-    if
-        beautyAndTheGalka == QUEST_ACCEPTED or
-        player:getCharVar("BeautyAndTheGalkaDenied") >= 1
-    then
-        player:startEvent(4)
-    elseif shadyBusiness == QUEST_COMPLETED then
-        player:startEvent(90)
-    elseif beautyAndTheGalka == QUEST_AVAILABLE then
-        player:startEvent(2)
-    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if csid == 2 and option == 0 then
-        player:addQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BEAUTY_AND_THE_GALKA)
-    elseif csid == 2 and option == 1 then
-        player:setCharVar("BeautyAndTheGalkaDenied", 1)
-    elseif csid == 3 then
-        player:tradeComplete()
-        player:addKeyItem(xi.ki.PALBOROUGH_MINES_LOGS)
-        player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.PALBOROUGH_MINES_LOGS)
-    elseif csid == 90 then
-        local ShadyBusiness = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.SHADY_BUSINESS)
-
-        if ShadyBusiness == QUEST_AVAILABLE then
-            player:addQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.SHADY_BUSINESS)
-        end
-    elseif csid == 91 then
-        local ShadyBusiness = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.SHADY_BUSINESS)
-
-        if ShadyBusiness == QUEST_ACCEPTED then
-            player:addFame(xi.quest.fame_area.NORG, 100)
-            player:completeQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.SHADY_BUSINESS)
-        else
-            player:addFame(xi.quest.fame_area.NORG, 80)
-        end
-
-        player:tradeComplete()
-        player:addGil(xi.settings.main.GIL_RATE * 350)
-        player:messageSpecial(ID.text.GIL_OBTAINED, xi.settings.main.GIL_RATE * 350)
-    end
-
 end
 
 return entity
