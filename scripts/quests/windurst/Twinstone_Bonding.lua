5e31707d20fe7f743c42454c9b7465f5abbc2da2
Wayne Work <animagic@gmail.com>
2023-01-04 12:16:33 -0500

    Twinstone Bonding quest revamped to new quest framework

---
+++ scripts/quests/windurst/Twinstone_Bonding.lua
@@ -0,0 +1,150 @@
+-----------------------------------
+-- Twinstone Bonding
+-----------------------------------
+-- !addquest 2 53
+-- Sigismund : !pos -110 -9 80 206
+-----------------------------------
+require('scripts/globals/interaction/quest')
+require("scripts/globals/items")
+require("scripts/globals/npc_util")
+require('scripts/globals/quests')
+require('scripts/globals/titles')
+require('scripts/globals/zone')
+
+-----------------------------------
+local quest = Quest:new(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TWINSTONE_BONDING)
+
+local function checkTrade(player, trade)
+    if
+        quest:getVar(player, "Prog") ~= 1 or
+        player:needToZone() and
+        npcUtil.tradeHasExactly(trade, { { xi.items.TWINSTONE_EARRING, 1 } })
+    then
+        return quest:progressEvent(490)
+    end
+end
+
+local function checkAndcompleteQuest(player)
+    if quest:complete(player) then
+        player:confirmTrade()
+        player:needToZone(true)
+    end
+end
+
+local function setInitialReward()
+    quest.reward =
+    {
+        fame = 80,
+        fameArea = xi.quest.fame_area.WINDURST,
+        item = xi.items.WRAPPED_BOW,
+        title = xi.title.BOND_FIXER
+    }
+end
+
+-- Repeat turn ins only get gil + less fame than the initial turn in
+local function setRepeatReward()
+    quest.reward =
+    {
+        fame = 10,
+        fameArea = xi.quest.fame_area.WINDURST,
+        gil = 900 * xi.settings.main.GIL_RATE
+    }
+end
+
+
+quest.sections =
+{
+    -- Quest Acceptance
+    {
+        check = function(player, status, vars)
+            return
+                player:getFameLevel(xi.quest.fame_area.WINDURST) >= 2 and
+                status == QUEST_AVAILABLE
+        end,
+
+        [xi.zone.WINDURST_WOODS] =
+        {
+            ['Gioh_Ajihri'] = quest:progressEvent(487, 0, xi.items.TWINSTONE_EARRING),
+
+            onEventFinish =
+            {
+                [487] = function(player, csid, option, npc)
+                    quest:begin(player)
+                    quest:setVar(player, 'Prog', 1)
+                end,
+            },
+        },
+    },
+
+    -- Quest Initial Completion
+    {
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED
+        end,
+
+        [xi.zone.WINDURST_WOODS] =
+        {
+            ['Gioh_Ajihri'] =
+            {
+                onTrigger = function(player, npc)
+                    return quest:progressEvent(488, 0, xi.items.TWINSTONE_EARRING)
+                end,
+
+                onTrade = function(player, npc, trade)
+                    return checkTrade(player, trade)
+                end
+            },
+
+            onEventFinish =
+            {
+                [488] = function(player, csid, option, npc)
+                    quest:setVar(player, "Prog", 1)
+                end,
+
+                [490] = function(player, csid, option, npc)
+                    setInitialReward()
+                    checkAndcompleteQuest(player)
+                end
+            },
+        },
+    },
+
+    -- Quest Repeat Completion
+    {
+        check = function(player, status, vars)
+            return status == QUEST_COMPLETED
+        end,
+
+        [xi.zone.PORT_WINDURST] =
+        {
+            ['Gioh_Ajihri'] =
+            {
+                onTrigger = function(player, npc)
+                    local item = xi.items.TWINSTONE_EARRING
+                    if player:needToZone() then
+                        return quest:event(491, 0, item)
+                    end
+                    return quest:progressEvent(488, 0, item)
+                end,
+
+                onTrade = function(player, npc, trade)
+                    return checkTrade(player, trade)
+                end
+            },
+
+            onEventFinish =
+            {
+                [488] = function(player, csid, option, npc)
+                    quest:setVar(player, "Prog", 1)
+                end,
+
+                [490] = function(player, csid, option, npc)
+                    setRepeatReward()
+                    checkAndcompleteQuest(player)
+                end
+            },
+        }
+    },
+}
+
+return quest
