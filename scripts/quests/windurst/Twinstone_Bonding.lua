745ba5d156fa901c71d5dfb6a063c901d31fac45
Wayne Work <animagic@gmail.com>
2023-01-08 20:13:52 -0500

    Finished converting to the new quest framework

--- scripts/quests/windurst/Twinstone_Bonding.lua
+++ scripts/quests/windurst/Twinstone_Bonding.lua
@@ -14,24 +14,13 @@ require('scripts/globals/zone')
 -----------------------------------
 local quest = Quest:new(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TWINSTONE_BONDING)
 
-quest.reward = {}
-
-local function checkTrade(player, trade)
-    if
-        quest:getVar(player, 'Prog') == 1 and
-        not player:needToZone() and
-        npcUtil.tradeHasExactly(trade, { { xi.items.TWINSTONE_EARRING, 1 } })
-    then
-        return quest:progressEvent(490)
-    end
-end
-
-local function checkAndcompleteQuest(player)
-    if quest:complete(player) then
-        player:confirmTrade()
-        player:needToZone(true)
-    end
-end
+quest.reward =
+{
+    fame = 80,
+    fameArea = xi.quest.fame_area.WINDURST,
+    item = xi.items.WRAPPED_BOW,
+    title = xi.title.BOND_FIXER
+}
 
 quest.sections =
 {
@@ -60,47 +49,7 @@ quest.sections =
     -- Quest Initial Completion
     {
         check = function(player, status, vars)
-            return status == QUEST_ACCEPTED
-        end,
-
-        [xi.zone.WINDURST_WOODS] =
-        {
-            ['Gioh_Ajihri'] =
-            {
-                onTrigger = function(player, npc)
-                    return quest:progressEvent(488, 0, xi.items.TWINSTONE_EARRING)
-                end,
-
-                onTrade = function(player, npc, trade)
-                    return checkTrade(player, trade)
-                end
-            },
-
-            onEventFinish =
-            {
-                [488] = function(player, csid, option, npc)
-                    quest:setVar(player, 'Prog', 1)
-                end,
-
-                [490] = function(player, csid, option, npc)
-                    player:messageSpecial(7251)
-                    quest.reward =
-                    {
-                        fame = 80,
-                        fameArea = xi.quest.fame_area.WINDURST,
-                        item = xi.items.WRAPPED_BOW,
-                        title = xi.title.BOND_FIXER
-                    }
-                    checkAndcompleteQuest(player)
-                end
-            },
-        },
-    },
-
-    -- Quest Repeat Completion
-    {
-        check = function(player, status, vars)
-            return status == QUEST_COMPLETED
+            return status >= QUEST_ACCEPTED
         end,
 
         [xi.zone.WINDURST_WOODS] =
@@ -116,7 +65,13 @@ quest.sections =
                 end,
 
                 onTrade = function(player, npc, trade)
-                    return checkTrade(player, trade)
+                    if
+                        quest:getVar(player, 'Prog') == 1 and
+                        not player:needToZone() and
+                        npcUtil.tradeHasExactly(trade, { { xi.items.TWINSTONE_EARRING, 1 } })
+                    then
+                        return quest:progressEvent(490)
+                    end
                 end
             },
 
@@ -127,17 +82,22 @@ quest.sections =
                 end,
 
                 [490] = function(player, csid, option, npc)
-                    player:messageSpecial(6649)
-                    quest.reward =
-                    {
-                        fame = 10,
-                        fameArea = xi.quest.fame_area.WINDURST,
-                        gil = 900 * xi.settings.main.GIL_RATE
-                    }
-                    checkAndcompleteQuest(player)
+                    if player:getQuestStatus(quest.areaId, quest.questId) == QUEST_COMPLETED then
+                        quest.reward =
+                        {
+                            fame = 10,
+                            fameArea = xi.quest.fame_area.WINDURST,
+                            gil = 900 * xi.settings.main.GIL_RATE
+                        }
+                    end
+
+                    if quest:complete(player) then
+                        player:confirmTrade()
+                        player:needToZone(true)
+                    end
                 end
             },
-        }
+        },
     },
 }
 
