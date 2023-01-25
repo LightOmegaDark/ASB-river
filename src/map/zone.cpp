08a6b0e943e464a9a60ab3a2f65f7e8b992bf7dd
Zach Toogood <zrtoogood@gmail.com>
2023-01-25 22:54:33 +0000

    Merge pull request #3524 from LandSandBoat/port_eden_los
    [los] Port Eden's line-of-sight system

--- src/map/zone.cpp
+++ src/map/zone.cpp
@@ -135,6 +135,7 @@ CZone::CZone(ZONEID ZoneID, REGION_TYPE RegionID, CONTINENT_TYPE ContinentID, ui
 , m_levelRestriction(levelRestriction)
 {
     TracyZoneScoped;
+
     m_useNavMesh = false;
     std::ignore  = m_useNavMesh;
     ZoneTimer    = nullptr;
@@ -159,7 +160,8 @@ CZone::CZone(ZONEID ZoneID, REGION_TYPE RegionID, CONTINENT_TYPE ContinentID, ui
 
     LoadZoneLines();
     LoadZoneWeather();
-    LoadNavMesh();
+
+    // NOTE: Heavy resources like Navmesh are now loaded outside of the constructor in zoneutils::LoadZoneList
 }
 
 CZone::~CZone()
@@ -517,6 +519,23 @@ void CZone::LoadNavMesh()
         DebugNavmesh("CZone::LoadNavMesh: Cannot load navmesh file (%s)", file);
         destroy(m_navMesh);
     }
+}
+
+void CZone::LoadZoneLos()
+{
+    if (GetType() == ZONE_TYPE::CITY || (m_miscMask & MISC_LOS_OFF))
+    {
+        // Skip cities and zones with line of sight turned off
+        return;
+    }
+
+    if (lineOfSight)
+    {
+        // Clean up previous object if one exists.
+        delete lineOfSight;
+    }
+
+    lineOfSight = ZoneLos::Load((uint16)GetID(), fmt::sprintf("losmeshes/%s.obj", GetName()));
 }
 
 /************************************************************************
