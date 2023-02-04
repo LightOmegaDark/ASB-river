2892cc6edd573b82fd004908ed86d81c9d8ef6dd
Loxley <119976133+LoxleyXI@users.noreply.github.com>
2023-02-04 14:33:53 +0000

    Moved rewards to settings, added retire option

--- modules/custom/lua/ironman_npcs.lua
+++ modules/custom/lua/ironman_npcs.lua
@@ -21,25 +21,41 @@ local ironmanDialog = {
     MENU_REWARD = {
         TITLE = "Choose an Ironman reward:",
     },
+    MENU_CURRENT = {
+        TITLE = "How goes your journey?",
+        REWARDS = "Collect a reward",
+        RETIRE = "Retire from Ironman",
+        NOTHING = "Nothing",
+    },
+    MENU_RETIRE = {
+        TITLE = "Are you absolutely sure?",
+        AGREE = "Yes",
+        DECLINE = "No",
+    },
     PREVENTED = "It's too late, adventurer. You've already taken another path.",
     ACCEPTED1 = "This path will be long and difficult. It will take an iron will to see it through",
+    RETIRE_WARNING = "Warning: You will no longer be an Ironman or eligible for Ironman rewards. This cannot be undone.",
+    RETIRE = "You are no longer an Ironman.",
+}
+
+-- TODO: Add pagination
+local ironmanRewards = {
+    REWARD_LV_18 = { lv = 18, id = xi.items.RABBIT_HIDE, desc = "Rabbit Hide" },
+    REWARD_LV_30 = { lv = 30, id = xi.items.WILD_ONION, desc = "Wild Onion" },
+    REWARD_LV_50 = { lv = 50, id = xi.items.XIPHOS, desc = "Xiphos" },
 }
 
 m:loadSettings(ironmanDialog)
-
-local rewards = {
-    { lv = 18, id = xi.items.RABBIT_HIDE, desc = "(Lv18) Rabbit Hide" },
-    { lv = 30, id = xi.items.XIPHOS, desc = "(Lv30) Xiphos" },
-}
+m:loadSettings(ironmanRewards)
 
 local function setIronmanRewards(player)
     local options = {}
     local playerLevel = player:getMainLvl()
-    for _, v in pairs(rewards) do
+    for _, v in pairs(ironmanRewards) do
         if playerLevel >= v.lv then
             table.insert(options, {
-                v.desc, function(playerArg)
-                    npcUtil.giveItem(player, v.id)
+                string.format("(%d) %s", v.lv, v.desc), function(playerArg)
+                    npcUtil.giveItem(playerArg, v.id)
                 end
             })
         end
@@ -47,38 +63,74 @@ local function setIronmanRewards(player)
     return options
 end
 
+local function menuRewards(player, npc)
+    local menuReward = {
+        title = ironmanDialog.MENU_REWARD.TITLE,
+        options = {},
+    }
+    menuReward.options = setIronmanRewards(player)
+    player:customMenu(menuReward)
+end
+
+local function menuRetire(player, npc)
+    player:PrintToPlayer(ironmanDialog.RETIRE_WARNING, xi.msg.channel.SYSTEM_3)
+    player:customMenu({
+        title = ironmanDialog.MENU_RETIRE.TITLE,
+        options = {
+            {
+                ironmanDialog.MENU_RETIRE.AGREE,
+                function(playerArg, npc)
+                    playerArg:PrintToPlayer(ironmanDialog.RETIRED, xi.msg.channel.SYSTEM_3)
+                    playerArg:setVar(CHAR_RESTRICTION, 0)
+                end
+            },
+            { ironmanDialog.MENU_RETIRE.DECLINE, function() end },
+        }
+    })
+end
+
+local function menuCurrent(player, npc)
+    player:customMenu({
+        title = ironmanDialog.MENU_CURRENT.TITLE,
+        options = {
+            { ironmanDialog.MENU_CURRENT.REWARDS, menuRewards },
+            { ironmanDialog.MENU_CURRENT.RETIRE, menuRetire },
+            { ironmanDialog.MENU_CURRENT.NOTHING, function() end }
+        }
+    })
+end
+
+local function menuAccept(player, npc)
+    player:PrintToPlayer(ironmanDialog.WARNING1, xi.msg.channel.SYSTEM_3)
+    player:PrintToPlayer(ironmanDialog.WARNING2, xi.msg.channel.SYSTEM_3)
+    player:customMenu({
+        title = ironmanDialog.MENU_ACCEPT.TITLE,
+        options = {
+            {
+                ironmanDialog.MENU_ACCEPT.AGREE,
+                function(playerArg, npc)
+                    playerArg:setVar(CHAR_INTERACTED, 1)
+                    playerArg:setVar(CHAR_RESTRICTION, 255)
+                    playerArg:PrintToPlayer(ironmanDialog.ACCEPTED1, xi.msg.channel.NS_SAY, npc:getPacketName())
+                    playerArg:PrintToPlayer(ironmanDialog.ACCEPTED2, xi.msg.channel.SYSTEM_3)
+                end
+            },
+            {
+                ironmanDialog.MENU_ACCEPT.DECLINE,
+                function(playerArg, npc)
+                    playerArg:PrintToPlayer(ironmanDialog.MENU_ACCEPT.EXIT, xi.msg.channel.NS_SAY, npc:getPacketName())
+                end
+            }
+        }
+    })
+end
+
 local function ironmanOnTrigger(player, npc)
     if player:getVar(CHAR_RESTRICTION) ~= 0 then
-        local menuReward = {
-            title = ironmanDialog.MENU_REWARD.TITLE,
-            options = {},
-        }
-        menuReward.options = setIronmanRewards(player)
-        player:customMenu(menuReward)
+        menuCurrent(player, npc)
     else
         if player:getVar(CHAR_INTERACTED) == 0 then
-            player:PrintToPlayer(ironmanDialog.WARNING1, xi.msg.channel.SYSTEM_3)
-            player:PrintToPlayer(ironmanDialog.WARNING2, xi.msg.channel.SYSTEM_3)
-            player:customMenu({
-                title = ironmanDialog.MENU_ACCEPT.TITLE,
-                options = {
-                    {
-                        ironmanDialog.MENU_ACCEPT.AGREE,
-                        function(playerArg, npc)
-                            playerArg:setVar(CHAR_INTERACTED, 1)
-                            playerArg:setVar(CHAR_RESTRICTION, 255)
-                            playerArg:PrintToPlayer(ironmanDialog.ACCEPTED1, xi.msg.channel.NS_SAY, npc:getPacketName())
-                            playerArg:PrintToPlayer(ironmanDialog.ACCEPTED2, xi.msg.channel.SYSTEM_3)
-                        end
-                    },
-                    {
-                        ironmanDialog.MENU_ACCEPT.DECLINE,
-                        function(playerArg, npc)
-                            playerArg:PrintToPlayer(ironmanDialog.MENU_ACCEPT.EXIT, xi.msg.channel.NS_SAY, npc:getPacketName())
-                        end
-                    }
-                }
-            })
+            menuAccept(player, npc)
         else
             player:PrintToPlayer(ironmanDialog.PREVENTED, xi.msg.channel.NS_SAY, npc:getPacketName())
         end
