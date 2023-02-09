Wings-XI/LegacyWingsXI

Various Spell Fixes

scripts/globals/spells/bio_iii.lua
@@ -43,7 +43,7 @@ function onSpellCast(caster, target, spell)
     local final = finalMagicAdjustments(caster, target, spell, dmg)
 
     -- Calculate duration
-    local duration = calculateDuration(180, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    local merits = caster:getMerit(tpz.merit.BIO_III)
 
     -- Calculate DoT effect
     -- http://wiki.ffo.jp/html/1954.html
@@ -67,7 +67,7 @@ function onSpellCast(caster, target, spell)
 
     -- Do it!
     target:delStatusEffect(tpz.effect.DIA)
-    target:addStatusEffect(tpz.effect.BIO, dotdmg, 3, duration, 0, 20, 3)
+    target:addStatusEffect(tpz.effect.BIO, dotdmg, 3, merits, 0, 20, 3)
     spell:setMsg(tpz.msg.basic.MAGIC_DMG)
 
     return final

scripts/globals/spells/dia_iii.lua
@@ -46,12 +46,12 @@ function onSpellCast(caster, target, spell)
     local final = finalMagicAdjustments(caster, target, spell, dmg)
 
     -- Calculate duration and bonus
-    local duration = calculateDuration(180, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    local merits = caster:getMerit(tpz.merit.DIA_III)
     local dotBonus = caster:getMod(tpz.mod.DIA_DOT) -- Dia Wand
 
     -- Do it!
     target:delStatusEffect(tpz.effect.BIO)
-    target:addStatusEffect(tpz.effect.DIA, 3 + dotBonus, 3, duration, 0, 20, 3)
+    target:addStatusEffect(tpz.effect.DIA, 3 + dotBonus, 3, merits, 0, 20, 3)
     spell:setMsg(tpz.msg.basic.MAGIC_DMG)
 
     return final

LightOmegaDark/ASB-river

scripts/globals/spells/black/bio_iii.lua
@@ -42,7 +42,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     local final = finalMagicAdjustments(caster, target, spell, dmg)
 
     -- Calculate duration
-    local duration = calculateDuration(180, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    local merits = caster:getMerit(xi.merit.BIO_III)
 
     -- Check for Dia
     local dia = target:getStatusEffect(xi.effect.DIA)
@@ -68,7 +68,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     end
 
     -- Do it!
-    target:addStatusEffect(xi.effect.BIO, dotdmg, 3, duration, 0, 20, 3)
+    target:addStatusEffect(xi.effect.BIO, dotdmg, 3, merits, 0, 20, 3)
     spell:setMsg(xi.msg.basic.MAGIC_DMG)
 
     -- Try to kill same tier Dia (default behavior)

scripts/globals/spells/white/dia_iii.lua
@@ -40,7 +40,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     local final = finalMagicAdjustments(caster, target, spell, dmg)
 
     -- Calculate duration and bonus
-    local duration = calculateDuration(180, spell:getSkillType(), spell:getSpellGroup(), caster, target)
+    local merits = caster:getMerit(xi.merit.DIA_III)
     local dotBonus = caster:getMod(xi.mod.DIA_DOT) -- Dia Wand
 
     spell:setMsg(xi.msg.basic.MAGIC_DMG) -- hit for initial damage
@@ -48,13 +48,13 @@ spellObject.onSpellCast = function(caster, target, spell)
     local bio = target:getStatusEffect(xi.effect.BIO)
 
     if  bio == nil then -- if no bio, just add dia dot
-        target:addStatusEffect(xi.effect.DIA, 3 + dotBonus, 3, duration, 0, 20, 3)
+        target:addStatusEffect(xi.effect.DIA, 3 + dotBonus, 3, merits, 0, 20, 3)
     elseif
         bio:getSubPower() <= 15 or
         (xi.settings.main.BIO_OVERWRITE == 1 and bio:getSubPower() <= 20) -- also erase same tier bio if BIO_OVERWRITE option is on (non-default)
     then -- erase lower tier bio and add dia dot
         target:delStatusEffect(xi.effect.BIO)
-        target:addStatusEffect(xi.effect.DIA, 3 + dotBonus, 3, duration, 0, 20, 3)
+        target:addStatusEffect(xi.effect.DIA, 3 + dotBonus, 3, merits, 0, 20, 3)
     end
 
     return final