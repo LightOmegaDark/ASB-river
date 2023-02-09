Wings-XI/LegacyWingsXI

Fix an issue with Drain II

scripts/globals/spells/drain_ii.lua
@@ -47,9 +47,13 @@ function onSpellCast(caster, target, spell)
     dmg = finalMagicAdjustments(caster, target, spell, dmg)
 
     local leftOver = (caster:getHP() + dmg) - caster:getMaxHP()
+    local overHeal = (leftOver/caster:getMaxHP())*100
+    if caster:hasStatusEffect(tpz.effect.WEAKNESS) then
+        overHeal = (leftOver/(caster:getBaseHP() + caster:getMod(tpz.mod.HP)))*100
+    end
 
     if (leftOver > 0) then
-        caster:addStatusEffect(tpz.effect.MAX_HP_BOOST, (leftOver/caster:getMaxHP())*100, 0, 60)
+        caster:addStatusEffect(tpz.effect.MAX_HP_BOOST, overHeal, 0, 60)
     end
 
     dmg = caster:addHP(dmg)

LightOmegaDark/ASB-river

scripts/globals/spells/black/drain_ii.lua
@@ -49,9 +49,13 @@ spellObject.onSpellCast = function(caster, target, spell)
     dmg = finalMagicAdjustments(caster, target, spell, dmg)
 
     local leftOver = (caster:getHP() + dmg) - caster:getMaxHP()
+    local overHeal = (leftOver/caster:getMaxHP())*100
+    if caster:hasStatusEffect(xi.effect.WEAKNESS) then
+        overHeal = (leftOver/(caster:getBaseHP() + caster:getMod(xi.mod.HP)))*100
+    end
 
-    if leftOver > 0 then
-        caster:addStatusEffect(xi.effect.MAX_HP_BOOST, (leftOver / caster:getMaxHP()) * 100, 0, 60)
+    if (leftOver > 0) then
+        caster:addStatusEffect(xi.effect.MAX_HP_BOOST, overHeal, 0, 60)
     end
 
     caster:addHP(dmg)