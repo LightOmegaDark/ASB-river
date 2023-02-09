Wings-XI/LegacyWingsXI

Fix Enspell Enhancement Items

scripts/globals/items/aero_mufflers.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENAERO
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end

scripts/globals/items/blizzard_gloves.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENBLIZZARD
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end

scripts/globals/items/fire_bracers.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENFIRE
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end

scripts/globals/items/stone_bangles.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENSTONE
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end

scripts/globals/items/thunder_mittens.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENTHUNDER
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end

scripts/globals/items/water_mitts.lua
@@ -10,5 +10,17 @@ end
 
 function onItemUse(target)
     local effect = tpz.effect.ENWATER
-    doEnspell(target, target, nil, effect)
+    local duration = 180
+    local magicskill = target:getSkillLevel(tpz.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end

LightOmegaDark/ASB-river

scripts/globals/items/aero_mufflers.lua
@@ -11,7 +11,18 @@ end
 
 itemObject.onItemUse = function(target)
     local effect = xi.effect.ENAERO
-    doEnspell(target, target, nil, effect)
+    local magicskill = target:getSkillLevel(xi.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
 
 return itemObject

scripts/globals/items/blizzard_gloves.lua
@@ -11,7 +11,18 @@ end
 
 itemObject.onItemUse = function(target)
     local effect = xi.effect.ENBLIZZARD
-    doEnspell(target, target, nil, effect)
+    local magicskill = target:getSkillLevel(xi.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
 
 return itemObject

scripts/globals/items/fire_bracers.lua
@@ -11,7 +11,18 @@ end
 
 itemObject.onItemUse = function(target)
     local effect = xi.effect.ENFIRE
-    doEnspell(target, target, nil, effect)
+    local magicskill = target:getSkillLevel(xi.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
 
 return itemObject

scripts/globals/items/stone_bangles.lua
@@ -11,7 +11,18 @@ end
 
 itemObject.onItemUse = function(target)
     local effect = xi.effect.ENSTONE
-    doEnspell(target, target, nil, effect)
+    local magicskill = target:getSkillLevel(xi.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
 
 return itemObject

scripts/globals/items/thunder_mittens.lua
@@ -11,7 +11,18 @@ end
 
 itemObject.onItemUse = function(target)
     local effect = xi.effect.ENTHUNDER
-    doEnspell(target, target, nil, effect)
+    local magicskill = target:getSkillLevel(xi.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
 
 return itemObject

scripts/globals/items/water_mitts.lua
@@ -11,7 +11,18 @@ end
 
 itemObject.onItemUse = function(target)
     local effect = xi.effect.ENWATER
-    doEnspell(target, target, nil, effect)
+    local magicskill = target:getSkillLevel(xi.skill.ENHANCING_MAGIC)
+    local potency = 0
+
+    if magicskill <= 200 then
+        potency = 3 + math.floor(6 * magicskill / 100)
+    elseif magicskill > 200 then
+        potency = 5 + math.floor(5 * magicskill / 100)
+    end
+
+    potency = utils.clamp(potency, 3, 25)
+
+    target:addStatusEffect(effect, potency, 0, 180)
 end
 
 return itemObject