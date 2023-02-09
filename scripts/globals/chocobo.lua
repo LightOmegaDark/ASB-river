Wings-XI/LegacyWingsXI

Add Chocobo Ticket and Chocopass Functionality to Chocobo Renter NPCs

scripts/globals/chocobo.lua
@@ -113,38 +113,93 @@ tpz.chocobo.renterOnTrigger = function(player, eventSucceed, eventFail)
     end
 end
 
+tpz.chocobo.renterOnTrade = function(player, npc, trade, eventSucceed, eventFail)
+    local zoneId = player:getZoneID()
+    local info   = chocoboInfo[zoneId]
+
+    if trade:hasItemQty(1789, 1) and zoneId == (tpz.zone.WINDURST_WOODS or tpz.zone.BASTOK_MINES or tpz.zone.SOUTHERN_SAN_DORIA) then -- Chocopass Fails for Non-Starter Cities
+        if info.past then -- Fails for Past Zones
+            player:startEvent(eventFail)
+        else
+            local currency = player:getGil()
+            local price = 0
+
+            player:tradeComplete()
+            player:setLocalVar("Chocopass", 1)
+            player:setLocalVar("ChocopassDuration", 120)
+            player:startEvent(eventSucceed, price, currency)
+        end
+    elseif trade:hasItemQty(1514, 1) then -- Chocobo Ticket
+        if info.past then -- Fails for Past Zones
+            player:startEvent(eventFail)
+        else
+            local mLvl     = player:getMainLvl()
+            local currency = player:getGil()
+            local price    = 0
+            local duration = 1800 + (player:getMod(tpz.mod.CHOCOBO_RIDING_TIME) * 60)
+
+            if mLvl >= 20 and player:hasKeyItem(tpz.ki.CHOCOBO_LICENSE) then
+                
+                player:tradeComplete()
+                player:setLocalVar("Chocopass", 1)
+                player:setLocalVar("ChocopassDuration", duration)
+                player:startEvent(eventSucceed, price, currency)
+            else
+                player:startEvent(eventFail) -- Fail Closed
+            end
+        end
+    else
+        player:startEvent(eventFail) -- Fail Closed
+    end
+end
+
 tpz.chocobo.renterOnEventFinish = function(player, csid, option, eventSucceed)
     if csid == eventSucceed and option == 0 then
         local mLvl   = player:getMainLvl()
         local zoneId = player:getZoneID()
         local info   = chocoboInfo[zoneId]
-
+        local trade  = player:getLocalVar("Chocopass")
+        
         if info then
-            local price = player:getLocalVar("[CHOCOBO]price")
-            player:setLocalVar("[CHOCOBO]price", 0)
-
-            if price and (info.past and player:getCurrency("allied_notes") >= price) or (not info.past and player:delGil(price)) then
-                if info.past then
-                    player:delCurrency("allied_notes", price)
-                end
-
-                updatePrice(zoneId, info, price)
-
-                local duration = 900
-                if mLvl >= 20 then
-                    duration = 1800 + (player:getMod(tpz.mod.CHOCOBO_RIDING_TIME) * 60)
-                end
+            if trade == 1 then
+                local duration = player:getLocalVar("ChocopassDuration")
 
                 player:addStatusEffectEx(tpz.effect.MOUNTED, tpz.effect.MOUNTED, 0, 0, duration, true)
 
                 if info.pos then
                     player:setPos(unpack(info.pos))
                 end
+
+                player:setLocalVar("Chocopass", 0)
+                player:setLocalVar("ChocopassDuration", 0)
+
+            elseif trade ~=1 then
+            local price = player:getLocalVar("[CHOCOBO]price")
+            player:setLocalVar("[CHOCOBO]price", 0)
+
+                if price and (info.past and player:getCurrency("allied_notes") >= price) or (not info.past and player:delGil(price)) then
+                    if info.past then
+                        player:delCurrency("allied_notes", price)
+                    end
+
+                    updatePrice(zoneId, info, price)
+
+                    local duration = 900
+                    if mLvl >= 20 then
+                        duration = 1800 + (player:getMod(tpz.mod.CHOCOBO_RIDING_TIME) * 60)
+                    end
+
+                    player:addStatusEffectEx(tpz.effect.MOUNTED, tpz.effect.MOUNTED, 0, 0, duration, true)
+
+                    if info.pos then
+                        player:setPos(unpack(info.pos))
+                    end
+                else
+                    printf("[warning] player %s reached succeed without enough currency in tpz.chocobo.renterOnEventFinish", player:getName())
+                end
             else
-                printf("[warning] player %s reached succeed without enough currency in tpz.chocobo.renterOnEventFinish", player:getName())
+                printf("[warning] player %s passed bad zoneId %i in tpz.chocobo.renterOnEventFinish", player:getName(), zoneId)       
             end
-        else
-            printf("[warning] player %s passed bad zoneId %i in tpz.chocobo.renterOnEventFinish", player:getName(), zoneId)
         end
     end
 end

scripts/zones/Bastok_Mines/npcs/Eulaphe.lua
@@ -11,6 +11,7 @@ local eventSucceed = 62
 local eventFail    = 65
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

scripts/zones/Konschtat_Highlands/npcs/Plaiaude.lua
@@ -11,6 +11,7 @@ local eventSucceed = 910
 local eventFail    = 911
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

scripts/zones/Meriphataud_Mountains_[S]/npcs/Lidaise.lua
@@ -11,6 +11,7 @@ local eventSucceed = 106
 local eventFail    = 107
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

scripts/zones/Rabao/npcs/Guinavie.lua
@@ -11,6 +11,7 @@ local eventSucceed = 79
 local eventFail    = 80
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

scripts/zones/Southern_San_dOria/npcs/Meuneille.lua
@@ -11,6 +11,7 @@ local eventSucceed = 601
 local eventFail    = 604
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

scripts/zones/Wajaom_Woodlands/npcs/Watisa.lua
@@ -11,6 +11,7 @@ local eventSucceed = 9
 local eventFail    = 10
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

scripts/zones/Windurst_Woods/npcs/Amimi.lua
@@ -11,6 +11,7 @@ local eventSucceed = 10004
 local eventFail    = 10007
 
 function onTrade(player, npc, trade)
+    tpz.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 function onTrigger(player, npc)

LightOmegaDark/ASB-river

scripts/globals/chocobo.lua
@@ -117,42 +117,93 @@ xi.chocobo.renterOnTrigger = function(player, eventSucceed, eventFail)
     end
 end
 
+xi.chocobo.renterOnTrade = function(player, npc, trade, eventSucceed, eventFail)
+    local zoneId = player:getZoneID()
+    local info   = chocoboInfo[zoneId]
+
+    if trade:hasItemQty(1789, 1) and zoneId == (xi.zone.WINDURST_WOODS or xi.zone.BASTOK_MINES or xi.zone.SOUTHERN_SAN_DORIA) then -- Chocopass Fails for Non-Starter Cities
+        if info.past then -- Fails for Past Zones
+            player:startEvent(eventFail)
+        else
+            local currency = player:getGil()
+            local price = 0
+
+            player:tradeComplete()
+            player:setLocalVar("Chocopass", 1)
+            player:setLocalVar("ChocopassDuration", 120)
+            player:startEvent(eventSucceed, price, currency)
+        end
+    elseif trade:hasItemQty(1514, 1) then -- Chocobo Ticket
+        if info.past then -- Fails for Past Zones
+            player:startEvent(eventFail)
+        else
+            local mLvl     = player:getMainLvl()
+            local currency = player:getGil()
+            local price    = 0
+            local duration = 1800 + (player:getMod(xi.mod.CHOCOBO_RIDING_TIME) * 60)
+
+            if mLvl >= 20 and player:hasKeyItem(xi.ki.CHOCOBO_LICENSE) then
+
+                player:tradeComplete()
+                player:setLocalVar("Chocopass", 1)
+                player:setLocalVar("ChocopassDuration", duration)
+                player:startEvent(eventSucceed, price, currency)
+            else
+                player:startEvent(eventFail) -- Fail Closed
+            end
+        end
+    else
+        player:startEvent(eventFail) -- Fail Closed
+    end
+end
+
 xi.chocobo.renterOnEventFinish = function(player, csid, option, eventSucceed)
     if csid == eventSucceed and option == 0 then
         local mLvl   = player:getMainLvl()
         local zoneId = player:getZoneID()
         local info   = chocoboInfo[zoneId]
+        local trade  = player:getLocalVar("Chocopass")
 
         if info then
-            local price = player:getLocalVar("[CHOCOBO]price")
-            player:setLocalVar("[CHOCOBO]price", 0)
-
-            if
-                price and
-                (info.past and player:getCurrency("allied_notes") >= price) or
-                (not info.past and player:delGil(price))
-            then
-                if info.past then
-                    player:delCurrency("allied_notes", price)
-                end
-
-                updatePrice(zoneId, info, price)
-
-                local duration = 900
-                if mLvl >= 20 then
-                    duration = 1800 + (player:getMod(xi.mod.CHOCOBO_RIDING_TIME) * 60)
-                end
+            if trade == 1 then
+                local duration = player:getLocalVar("ChocopassDuration")
 
                 player:addStatusEffectEx(xi.effect.MOUNTED, xi.effect.MOUNTED, 0, 0, duration, true)
 
                 if info.pos then
                     player:setPos(unpack(info.pos))
                 end
+
+                player:setLocalVar("Chocopass", 0)
+                player:setLocalVar("ChocopassDuration", 0)
+
+            elseif trade ~=1 then
+            local price = player:getLocalVar("[CHOCOBO]price")
+            player:setLocalVar("[CHOCOBO]price", 0)
+
+                if price and (info.past and player:getCurrency("allied_notes") >= price) or (not info.past and player:delGil(price)) then
+                    if info.past then
+                        player:delCurrency("allied_notes", price)
+                    end
+
+                    updatePrice(zoneId, info, price)
+
+                    local duration = 900
+                    if mLvl >= 20 then
+                        duration = 1800 + (player:getMod(xi.mod.CHOCOBO_RIDING_TIME) * 60)
+                    end
+
+                    player:addStatusEffectEx(xi.effect.MOUNTED, xi.effect.MOUNTED, 0, 0, duration, true)
+
+                    if info.pos then
+                        player:setPos(unpack(info.pos))
+                    end
+                else
+                    printf("[warning] player %s reached succeed without enough currency in xi.chocobo.renterOnEventFinish", player:getName())
+                end
             else
-                printf("[warning] player %s reached succeed without enough currency in xi.chocobo.renterOnEventFinish", player:getName())
+                printf("[warning] player %s passed bad zoneId %i in xi.chocobo.renterOnEventFinish", player:getName(), zoneId)
             end
-        else
-            printf("[warning] player %s passed bad zoneId %i in xi.chocobo.renterOnEventFinish", player:getName(), zoneId)
         end
     end
 end

scripts/zones/Bastok_Mines/npcs/Eulaphe.lua
@@ -12,6 +12,7 @@ local eventSucceed = 62
 local eventFail    = 65
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)

scripts/zones/Konschtat_Highlands/npcs/Plaiaude.lua
@@ -12,6 +12,7 @@ local eventSucceed = 910
 local eventFail    = 911
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)

scripts/zones/Meriphataud_Mountains_[S]/npcs/Lidaise.lua
@@ -12,6 +12,7 @@ local eventSucceed = 106
 local eventFail    = 107
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)

scripts/zones/Rabao/npcs/Guinavie.lua
@@ -12,6 +12,7 @@ local eventSucceed = 79
 local eventFail    = 80
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)

scripts/zones/Southern_San_dOria/npcs/Meuneille.lua
@@ -12,6 +12,7 @@ local eventSucceed = 601
 local eventFail    = 604
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)

scripts/zones/Wajaom_Woodlands/npcs/Watisa.lua
@@ -12,6 +12,7 @@ local eventSucceed = 9
 local eventFail    = 10
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)

scripts/zones/Windurst_Woods/npcs/Amimi.lua
@@ -12,6 +12,7 @@ local eventSucceed = 10004
 local eventFail    = 10007
 
 entity.onTrade = function(player, npc, trade)
+    xi.chocobo.renterOnTrade(player, npc, trade, eventSucceed, eventFail)
 end
 
 entity.onTrigger = function(player, npc)