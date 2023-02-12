e57ecbdc76d83ef5067315f812eaa6984d4cdea9
Loxley <119976133+LoxleyXI@users.noreply.github.com>
2023-02-12 13:20:14 +0000

    Fixed all Ironman NPC dialogs

--- modules/custom/lua/ironman_npcs.lua
+++ modules/custom/lua/ironman_npcs.lua
@@ -34,6 +34,7 @@ local ironmanDialog = {
     },
     PREVENTED = "It's too late, adventurer. You've already taken another path.",
     ACCEPTED1 = "This path will be long and difficult. It will take an iron will to see it through",
+    ACCEPTED2 = "You have become an Ironman.",
     RETIRE_WARNING = "Warning: You will no longer be an Ironman or eligible for Ironman rewards. This cannot be undone.",
     RETIRE = "You are no longer an Ironman.",
 }
@@ -47,6 +48,15 @@ local ironmanRewards = {
 
 m:loadSettings(ironmanDialog)
 m:loadSettings(ironmanRewards)
+
+local menu  = {}
+
+local delaySendMenu = function(player, menuNext)
+    menu = menuNext
+    player:timer(50, function(playerArg)
+        playerArg:customMenu(menu)
+    end)
+end
 
 local function setIronmanRewards(player)
     local options = {}
@@ -63,74 +73,81 @@ local function setIronmanRewards(player)
     return options
 end
 
-local function menuRewards(player, npc)
-    local menuReward = {
-        title = ironmanDialog.MENU_REWARD.TITLE,
-        options = {},
-    }
-    menuReward.options = setIronmanRewards(player)
-    player:customMenu(menuReward)
-end
+local MENU_REWARD = {
+    title = ironmanDialog.MENU_REWARD.TITLE,
+    options = {},
+}
 
-local function menuRetire(player, npc)
-    player:PrintToPlayer(ironmanDialog.RETIRE_WARNING, xi.msg.channel.SYSTEM_3)
-    player:customMenu({
-        title = ironmanDialog.MENU_RETIRE.TITLE,
-        options = {
-            {
-                ironmanDialog.MENU_RETIRE.AGREE,
-                function(playerArg, npc)
-                    playerArg:PrintToPlayer(ironmanDialog.RETIRED, xi.msg.channel.SYSTEM_3)
-                    playerArg:setVar(CHAR_RESTRICTION, 0)
-                end
-            },
-            { ironmanDialog.MENU_RETIRE.DECLINE, function() end },
-        }
-    })
-end
+local MENU_RETIRE = {
+    title = ironmanDialog.MENU_RETIRE.TITLE,
+    options = {
+        {
+            ironmanDialog.MENU_RETIRE.AGREE,
+            function(player)
+                player:PrintToPlayer(ironmanDialog.RETIRE, xi.msg.channel.SYSTEM_3)
+                player:setVar(CHAR_RESTRICTION, 0)
+            end,
+        },
+        {
+            ironmanDialog.MENU_RETIRE.DECLINE,
+            function() end,
+        },
+    },
+}
 
-local function menuCurrent(player, npc)
-    player:customMenu({
-        title = ironmanDialog.MENU_CURRENT.TITLE,
-        options = {
-            { ironmanDialog.MENU_CURRENT.REWARDS, menuRewards },
-            { ironmanDialog.MENU_CURRENT.RETIRE, menuRetire },
-            { ironmanDialog.MENU_CURRENT.NOTHING, function() end }
-        }
-    })
-end
+local MENU_CURRENT = {
+    title = ironmanDialog.MENU_CURRENT.TITLE,
+    options = {
+        {
+            ironmanDialog.MENU_CURRENT.REWARDS,
+            function(player)
+                MENU_REWARD.options = setIronmanRewards(player)
+                delaySendMenu(player, MENU_REWARD)
+            end,
+        },
+        {
+            ironmanDialog.MENU_CURRENT.RETIRE,
+            function(player)
+                player:PrintToPlayer(ironmanDialog.RETIRE_WARNING, xi.msg.channel.SYSTEM_3)
+                delaySendMenu(player, MENU_RETIRE)
+            end,
+        },
+        {
+            ironmanDialog.MENU_CURRENT.NOTHING,
+            function() end,
+        },
+    },
+}
 
-local function menuAccept(player, npc)
-    player:PrintToPlayer(ironmanDialog.WARNING1, xi.msg.channel.SYSTEM_3)
-    player:PrintToPlayer(ironmanDialog.WARNING2, xi.msg.channel.SYSTEM_3)
-    player:customMenu({
-        title = ironmanDialog.MENU_ACCEPT.TITLE,
-        options = {
-            {
-                ironmanDialog.MENU_ACCEPT.AGREE,
-                function(playerArg, npc)
-                    playerArg:setVar(CHAR_INTERACTED, 1)
-                    playerArg:setVar(CHAR_RESTRICTION, 255)
-                    playerArg:PrintToPlayer(ironmanDialog.ACCEPTED1, xi.msg.channel.NS_SAY, npc:getPacketName())
-                    playerArg:PrintToPlayer(ironmanDialog.ACCEPTED2, xi.msg.channel.SYSTEM_3)
-                end
-            },
-            {
-                ironmanDialog.MENU_ACCEPT.DECLINE,
-                function(playerArg, npc)
-                    playerArg:PrintToPlayer(ironmanDialog.MENU_ACCEPT.EXIT, xi.msg.channel.NS_SAY, npc:getPacketName())
-                end
-            }
-        }
-    })
-end
+local MENU_ACCEPT = {
+    title = ironmanDialog.MENU_ACCEPT.TITLE,
+    options = {
+        {
+            ironmanDialog.MENU_ACCEPT.AGREE,
+            function(player)
+                player:setVar(CHAR_INTERACTED, 1)
+                player:setVar(CHAR_RESTRICTION, 255)
+                player:PrintToPlayer(ironmanDialog.ACCEPTED1, xi.msg.channel.NS_SAY)
+                player:PrintToPlayer(ironmanDialog.ACCEPTED2, xi.msg.channel.SYSTEM_3)
+            end,
+        },
+        {
+            ironmanDialog.MENU_ACCEPT.DECLINE,
+            function(player)
+                player:PrintToPlayer(ironmanDialog.MENU_ACCEPT.EXIT, xi.msg.channel.NS_SAY)
+            end,
+        },
+    },
+}
 
 local function ironmanOnTrigger(player, npc)
     if player:getVar(CHAR_RESTRICTION) ~= 0 then
-        menuCurrent(player, npc)
+        delaySendMenu(player, MENU_CURRENT)
     else
         if player:getVar(CHAR_INTERACTED) == 0 then
-            menuAccept(player, npc)
+            player:PrintToPlayer(ironmanDialog.WARNING1, xi.msg.channel.SYSTEM_3)
+            player:PrintToPlayer(ironmanDialog.WARNING2, xi.msg.channel.SYSTEM_3)
+            delaySendMenu(player, MENU_ACCEPT)
         else
             player:PrintToPlayer(ironmanDialog.PREVENTED, xi.msg.channel.NS_SAY, npc:getPacketName())
         end
@@ -173,18 +190,24 @@ local IRONMAN_NPC = {
     }
 }
 
+-- Bastok Markets (Robin)
+-- !pos -192.5 -4 80 235
 m:addOverride("xi.zones.Bastok_Markets.Zone.onInitialize", function(zone)
     super(zone)
     local horro = zone:insertDynamicEntity(IRONMAN_NPC["Bastok_Markets"])
     utils.unused(horro)
 end)
 
+-- Port San d'Oria (Robinette)
+-- !pos -60 -4.5 -39 232
 m:addOverride("xi.zones.Port_San_dOria.Zone.onInitialize", function(zone)
     super(zone)
     local horro = zone:insertDynamicEntity(IRONMAN_NPC["Port_San_dOria"])
     utils.unused(horro)
 end)
 
+-- Windurst Walls (Robina)
+-- !pos 114.5 -11 171 239
 m:addOverride("xi.zones.Windurst_Walls.Zone.onInitialize", function(zone)
     super(zone)
     local horro = zone:insertDynamicEntity(IRONMAN_NPC["Windurst_Walls"])
