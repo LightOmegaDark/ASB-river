5b16c756c22c1919aa23d4c806d72f34ddfb1e55
Tsukuyomiii <114472598+The-Tsukuyomiii@users.noreply.github.com>
2023-03-31 17:26:28 -0400

    Revert "Update when_hell_freezes_over.lua"
    This reverts commit a42e529e0b6c6c3c19ae4a30376783c23ca93527.

--- scripts/zones/Bearclaw_Pinnacle/bcnms/when_hell_freezes_over.lua
+++ scripts/zones/Bearclaw_Pinnacle/bcnms/when_hell_freezes_over.lua
@@ -6,28 +6,28 @@
 require("scripts/globals/battlefield")
 require("scripts/globals/keyitems")
 -----------------------------------
-local battlefieldObject = {}
+local battlefield_object = {}
 
-battlefieldObject.onBattlefieldInitialise = function(battlefield)
+battlefield_object.onBattlefieldInitialise = function(battlefield)
     battlefield:setLocalVar("loot", 1)
     battlefield:setLocalVar("lootSpawned", 1)
     battlefield:setLocalVar("adds", 2)
 end
 
-battlefieldObject.onBattlefieldTick = function(battlefield, tick)
+battlefield_object.onBattlefieldTick = function(battlefield, tick)
     xi.battlefield.onBattlefieldTick(battlefield, tick)
 end
 
-battlefieldObject.onBattlefieldRegister = function(player, battlefield)
+battlefield_object.onBattlefieldRegister = function(player, battlefield)
 end
 
-battlefieldObject.onBattlefieldEnter = function(player, battlefield)
+battlefield_object.onBattlefieldEnter = function(player, battlefield)
     if player:hasKeyItem(xi.ki.ZEPHYR_FAN) then
         player:delKeyItem(xi.ki.ZEPHYR_FAN)
     end
 end
 
-battlefieldObject.onBattlefieldLeave = function(player, battlefield, leavecode)
+battlefield_object.onBattlefieldLeave = function(player, battlefield, leavecode)
     if leavecode == xi.battlefield.leaveCode.WON then
         local _, clearTime, partySize = battlefield:getRecord()
         player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), 0)
@@ -36,13 +36,13 @@ battlefieldObject.onBattlefieldLeave = function(player, battlefield, leavecode)
     end
 end
 
-battlefieldObject.onEventUpdate = function(player, csid, option)
+battlefield_object.onEventUpdate = function(player, csid, option)
 end
 
-battlefieldObject.onEventFinish = function(player, csid, option)
+battlefield_object.onEventFinish = function(player, csid, option)
     if csid == 32001 then
         player:addExp(3000)
     end
 end
 
-return battlefieldObject
+return battlefield_object
