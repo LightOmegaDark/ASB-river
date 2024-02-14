4313dbd402c378dccb70a8660e781ecef3c1c226
jamesbradleym <jamesbradleym@gmail.com>
2024-02-14 18:59:51 -0500

    fix logic and messages for quest

--- scripts/zones/Maze_of_Shakhrami/npcs/Rockwell.lua
+++ scripts/zones/Maze_of_Shakhrami/npcs/Rockwell.lua
@@ -11,16 +11,17 @@ require("scripts/globals/quests")
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    if
-        player:getQuestStatus(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.YOUR_CRYSTAL_BALL) == QUEST_ACCEPTED and
-        npcUtil.tradeHas(trade, xi.items.AHRIMAN_LENS)
-    then
-        if player:getCharVar("QuestYourCrystalBall_prog") > os.time() then
-            player:messageSpecial(ID.text.CANNOT_SUBMERGE)
-        else
-            player:setCharVar("QuestYourCrystalBall_prog", getMidnight())
-            player:confirmTrade(trade)
-            player:messageSpecial(ID.text.SUBMERGE)
+    if player:getQuestStatus(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.YOUR_CRYSTAL_BALL) == QUEST_ACCEPTED then
+        if npcUtil.tradeHasExactly(trade, { { xi.items.AHRIMAN_LENS, 1 } }) then
+            if player:getCharVar("QuestYourCrystalBall_prog") > os.time() then
+                player:messageSpecial(ID.text.CANNOT_SUBERMGE, xi.items.AHRIMAN_LENS)
+            elseif player:getCharVar("QuestYourCrystalBall_prog") == 0 then -- don't initiate submerge if previous one hasn't been collected
+                player:confirmTrade()
+                player:setCharVar("QuestYourCrystalBall_prog", getMidnight())
+                player:messageSpecial(ID.text.SUBMERGE, xi.items.AHRIMAN_LENS)
+            end
+        elseif npcUtil.tradeHas(trade, xi.items.AHRIMAN_LENS) then
+            player:messageSpecial(ID.text.CANNOT_SUBERMGE, xi.items.AHRIMAN_LENS)
         end
     end
 end
@@ -28,12 +29,13 @@ end
 entity.onTrigger = function(player, npc)
     if
         player:getQuestStatus(xi.quest.log_id.JEUNO, xi.quest.id.jeuno.YOUR_CRYSTAL_BALL) == QUEST_ACCEPTED and
-        player:getCharVar("QuestYourCrystalBall_prog") < os.time() and
-        not player:hasItem(xi.items.DIVINATION_SPHERE)
+        player:getCharVar("QuestYourCrystalBall_prog") ~= 0
     then
-        player:startEvent(52)
-    elseif player:getCharVar("QuestYourCrystalBall_prog") > os.time() then
-        player:messageSpecial(ID.text.NOT_READY, xi.items.DIVINATION_SPHERE)
+        if player:getCharVar("QuestYourCrystalBall_prog") < os.time() then
+            player:startEvent(52):progress()
+        elseif player:getCharVar("QuestYourCrystalBall_prog") > os.time() then
+            player:messageSpecial(ID.text.NOT_READY, 0, xi.items.DIVINATION_SPHERE):progress()
+        end
     end
 end
 
@@ -43,6 +45,7 @@ end
 entity.onEventFinish = function(player, csid, option)
     if csid == 52 then
         npcUtil.giveItem(player, xi.items.DIVINATION_SPHERE)
+        player:setCharVar("QuestYourCrystalBall_prog", 0)
     end
 end
 
